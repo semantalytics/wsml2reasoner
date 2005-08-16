ba29db1a3fc8d5a3a2a0a830d8bd9b4df6d4a03f
gabor <gabor@32303699-5c3f-0410-bc21-d627dcbb3c28>
2005-08-16 08:58:36 +0000

    Changed test to a real unit test.

--- test/reasoner/TestWSMLCoreReasoner.java
+++ test/reasoner/TestWSMLCoreReasoner.java
@@ -1,13 +1,25 @@
 package reasoner;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import java.io.*;
-import java.util.*;
+import junit.framework.TestCase;
 
-import org.deri.wsml.reasoner.api.*;
-import org.deri.wsml.reasoner.api.queryanswering.*;
+import org.deri.wsml.reasoner.api.Request;
+import org.deri.wsml.reasoner.api.WSMLReasoner;
+import org.deri.wsml.reasoner.api.WSMLReasonerFactory;
+import org.deri.wsml.reasoner.api.queryanswering.QueryAnsweringRequest;
+import org.deri.wsml.reasoner.api.queryanswering.QueryAnsweringResult;
+import org.deri.wsml.reasoner.api.queryanswering.VariableBinding;
 import org.deri.wsml.reasoner.impl.DefaultWSMLReasonerFactory;
 import org.deri.wsml.reasoner.impl.QueryAnsweringRequestImpl;
+import org.deri.wsml.reasoner.impl.VariableBindingImpl;
 import org.deri.wsmo4j.io.parser.wsml.LogExprParserImpl;
 import org.deri.wsmo4j.io.serializer.wsml.LogExprSerializerWSML;
 import org.omwg.logexpression.LogicalExpression;
@@ -16,129 +28,139 @@ import org.wsmo.common.TopEntity;
 import org.wsmo.common.exception.InvalidModelException;
 import org.wsmo.factory.Factory;
 import org.wsmo.factory.WsmoFactory;
-import org.wsmo.wsml.*;
+import org.wsmo.wsml.Parser;
+import org.wsmo.wsml.ParserException;
+import org.wsmo.wsml.Serializer;
 
+public class TestWSMLCoreReasoner extends TestCase {
 
-public class TestWSMLCoreReasoner {
+    private static final String NS = "http://www.example.org/example/#";
 
-    public static WSMLReasoner wsmlCoreReasoner;
-   
-    
-    private static void performQuery(Request r){
-        QueryAnsweringResult result = (QueryAnsweringResult) wsmlCoreReasoner.execute(r);
-        
-        System.out.println("Found < "+result.size()+" > results to the query:");
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(TestWSMLCoreReasoner.class);
+    }
+
+    private WSMLReasoner wsmlCoreReasoner;
+
+    private void performQuery(Request r, Set<VariableBinding> expected) {
+        QueryAnsweringResult result = (QueryAnsweringResult) wsmlCoreReasoner
+                .execute(r);
+
+        System.out.println("Found < " + result.size()
+                + " > results to the query:");
         int i = 0;
-        for(VariableBinding vBinding : result) {
-            System.out.println("("+ (++i) +") -- " + vBinding.toString());
+        for (VariableBinding vBinding : result) {
+            System.out.println("(" + (++i) + ") -- " + vBinding.toString());
+        }
+        assertEquals(result.size(), expected.size());
+        for (VariableBinding binding : expected) {
+            assertTrue(result.contains(binding));
         }
     }
-    
+
     /**
-     * Loads a simple ontology from a file, constructs a simple conjunctive query
-     * over the ontolgy, evaluates the query and prints the query answer to console.
+     * Loads a simple ontology from a file, constructs a simple conjunctive
+     * query over the ontolgy, evaluates the query and prints the query answer
+     * to console.
+     * 
      * @param args
      */
-    public static void main(String[] args) {
-        
+    public void testReasoner() {
+
         String ONTOLOGY_FILE = "examples/simple-graph.wsml";
         String PARSER_CLASS = "com.ontotext.wsmo4j.parser.WSMLParserImpl";
 
-        // Set up factories for creating WSML elements 
-        
+        // Set up factories for creating WSML elements
+
         Map<String, String> leProperties = new HashMap<String, String>();
         leProperties.put(Factory.PROVIDER_CLASS,
-         "org.deri.wsmo4j.logexpression.LogicalExpressionFactoryImpl");
+                "org.deri.wsmo4j.logexpression.LogicalExpressionFactoryImpl");
 
-        org.omwg.logexpression.LogicalExpressionFactory leFactory = 
-            (org.omwg.logexpression.LogicalExpressionFactory) Factory.createLogicalExpressionFactory(leProperties);
-        
+        org.omwg.logexpression.LogicalExpressionFactory leFactory = (org.omwg.logexpression.LogicalExpressionFactory) Factory
+                .createLogicalExpressionFactory(leProperties);
+
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put(Factory.PROVIDER_CLASS,
                 "com.ontotext.wsmo4j.factory.WsmoFactoryImpl");
         properties.put(Parser.PARSER_LE_FACTORY, leFactory);
         WsmoFactory factory = Factory.createWsmoFactory(properties);
-        
-        
+
         // Set up WSML parser
-        
+
         Map<String, Object> parserProperties = new HashMap<String, Object>();
         parserProperties.put(Parser.PARSER_WSMO_FACTORY, factory);
         parserProperties.put(Parser.PARSER_LE_FACTORY, leFactory);
-        
+
         parserProperties.put(org.wsmo.factory.Factory.PROVIDER_CLASS,
                 "com.ontotext.wsmo4j.parser.WSMLParserImpl");
 
-        Parser wsmlparserimpl = org.wsmo.factory.Factory.createParser(parserProperties);
-       
-        // Set up serializer 
-        
+        Parser wsmlparserimpl = org.wsmo.factory.Factory
+                .createParser(parserProperties);
+
+        // Set up serializer
+
         Map<String, String> serializerProperties = new HashMap<String, String>();
         serializerProperties.put(org.wsmo.factory.Factory.PROVIDER_CLASS,
                 "com.ontotext.wsmo4j.parser.WSMLSerializerImpl");
 
-        Serializer ontologySerializer = org.wsmo.factory.Factory.createSerializer(serializerProperties);
-        
+        Serializer ontologySerializer = org.wsmo.factory.Factory
+                .createSerializer(serializerProperties);
+
         // Read simple ontology from file
-        
+
         Ontology o = null;
-        
+
         try {
-            
+
             final Reader ontoReader = new FileReader(ONTOLOGY_FILE);
-            
-            
+
             final TopEntity[] identifiable = wsmlparserimpl.parse(ontoReader);
-            if (identifiable.length > 0
-                    && identifiable[0] instanceof Ontology) {
-                o = (Ontology)identifiable[0];
+            if (identifiable.length > 0 && identifiable[0] instanceof Ontology) {
+                o = (Ontology) identifiable[0];
             } else {
                 return;
             }
-            
-        }
-        catch (Exception e) {
-            System.out.println("Unable to parse ontology: "
-                    + e.getMessage());
+
+        } catch (Exception e) {
+            System.out.println("Unable to parse ontology: " + e.getMessage());
             return;
         }
-        
+
         // Print ontology in WSML
-        
+
         try {
             System.out.println("WSML Ontology:\n");
             StringWriter sw = new StringWriter();
-            ontologySerializer.serialize(new TopEntity[]{o}, sw);
+            ontologySerializer.serialize(new TopEntity[] { o }, sw);
             System.out.println(sw.toString());
             System.out.println("--------------\n\n");
         } catch (IOException e6) {
             // TODO Auto-generated catch block
             e6.printStackTrace();
         }
-        
-        org.omwg.logexpression.io.Serializer logExprSerializer = new LogExprSerializerWSML(o); 
-        
-        
-        // Build simple conjunctive query in WSML 
-        
+
+        org.omwg.logexpression.io.Serializer logExprSerializer = new LogExprSerializerWSML(
+                o);
+
+        // Build simple conjunctive query in WSML
+
         LogicalExpression qExpression1 = null;
         LogicalExpression qExpression2 = null;
         LogicalExpression qExpression3 = null;
-  
-        org.omwg.logexpression.io.Parser leParser = 
-            LogExprParserImpl.getInstance(o); // construct queries over the same ontology
-        
-         
+
+        org.omwg.logexpression.io.Parser leParser = LogExprParserImpl
+                .getInstance(o); // construct queries over the same ontology
+
         try {
             String query1 = "scElement(?n) and path(?n,f) and path(f,?n)";
             qExpression1 = leParser.parse(query1);
-                        
+
             String query2 = "path(?n,f)";
             qExpression2 = leParser.parse(query2);
-            
+
             String query3 = "path(?n1,?n2)";
             qExpression3 = leParser.parse(query3);
-            
+
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
@@ -153,9 +175,8 @@ public class TestWSMLCoreReasoner {
             return;
         }
 
-        
-        // Print query  
-        
+        // Print query
+
         System.out.println("WSML Query (1):");
         System.out.println(logExprSerializer.serialize(qExpression1));
         System.out.println("\nWSML Query (2):");
@@ -163,37 +184,227 @@ public class TestWSMLCoreReasoner {
         System.out.println("\nWSML Query (3):");
         System.out.println(logExprSerializer.serialize(qExpression3));
         System.out.println("--------------\n\n");
-        
-        // Now get a reasoner, create a query ansering request and print the result.
+
+        // Now get a reasoner, create a query ansering request and print the
+        // result.
         try {
-            
+
             Set<Ontology> ontos = new HashSet<Ontology>();
             ontos.add(o);
-            QueryAnsweringRequest qaRequest1 = new QueryAnsweringRequestImpl(ontos, qExpression1);
-            QueryAnsweringRequest qaRequest2 = new QueryAnsweringRequestImpl(ontos, qExpression2);
-            QueryAnsweringRequest qaRequest3 = new QueryAnsweringRequestImpl(ontos, qExpression3);
-            
-            wsmlCoreReasoner = DefaultWSMLReasonerFactory.getFactory().getWSMLReasoner(WSMLReasonerFactory.WSMLVariant.WSML_CORE);
-            
+            QueryAnsweringRequest qaRequest1 = new QueryAnsweringRequestImpl(
+                    ontos, qExpression1);
+            QueryAnsweringRequest qaRequest2 = new QueryAnsweringRequestImpl(
+                    ontos, qExpression2);
+            QueryAnsweringRequest qaRequest3 = new QueryAnsweringRequestImpl(
+                    ontos, qExpression3);
+
+            wsmlCoreReasoner = DefaultWSMLReasonerFactory.getFactory()
+                    .getWSMLReasoner(WSMLReasonerFactory.WSMLVariant.WSML_CORE);
+
             System.out.println("Starting reasoner with query (1) ...");
-            performQuery(qaRequest1);
+            Set<VariableBinding> expected = new HashSet<VariableBinding>();
+            VariableBinding binding = new VariableBindingImpl();
+            binding.put("n", NS + "f");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n", NS + "g");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n", NS + "h");
+            expected.add(binding);
+            performQuery(qaRequest1, expected);
             System.out.println("Finished query.");
-            
+
             System.out.println("Starting reasoner with query (2) ...");
-            performQuery(qaRequest2);
+            expected.clear();
+            binding = new VariableBindingImpl();
+            binding.put("n", NS + "a");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n", NS + "b");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n", NS + "c");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n", NS + "f");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n", NS + "g");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n", NS + "h");
+            expected.add(binding);
+            performQuery(qaRequest2, expected);
             System.out.println("Finished query.");
-            
+
             System.out.println("Starting reasoner with query (3) ...");
-            performQuery(qaRequest3);
+            expected.clear();
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "a");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "b");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "c");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "d");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "e");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "f");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "g");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "h");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "a");
+            binding.put("n2", NS + "i");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "a");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "b");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "c");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "d");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "e");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "f");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "g");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "h");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "b");
+            binding.put("n2", NS + "i");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "a");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "b");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "c");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "d");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "e");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "f");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "g");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "h");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "c");
+            binding.put("n2", NS + "i");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "f");
+            binding.put("n2", NS + "f");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "f");
+            binding.put("n2", NS + "g");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "f");
+            binding.put("n2", NS + "h");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "f");
+            binding.put("n2", NS + "i");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "g");
+            binding.put("n2", NS + "f");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "g");
+            binding.put("n2", NS + "g");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "g");
+            binding.put("n2", NS + "h");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "g");
+            binding.put("n2", NS + "i");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "h");
+            binding.put("n2", NS + "f");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "h");
+            binding.put("n2", NS + "g");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "h");
+            binding.put("n2", NS + "h");
+            expected.add(binding);
+            binding = new VariableBindingImpl();
+            binding.put("n1", NS + "h");
+            binding.put("n2", NS + "i");
+            expected.add(binding);
+            performQuery(qaRequest3, expected);
             System.out.println("Finished query.");
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         System.out.println("Finished!");
-        
-        
+
     }
 
 }
