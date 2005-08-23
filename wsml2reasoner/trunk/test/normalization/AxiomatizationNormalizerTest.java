a5b36cc2c84f41fea95eae4a3f21647a2ebc2a62
gabor <gabor@32303699-5c3f-0410-bc21-d627dcbb3c28>
2005-08-23 15:02:08 +0000

    *** empty log message ***

--- wsml2reasoner/trunk/test/normalization/AxiomatizationNormalizerTest.java
+++ wsml2reasoner/trunk/test/normalization/AxiomatizationNormalizerTest.java
@@ -1,41 +1,25 @@
 package normalization;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import junit.framework.TestCase;
-
 import org.deri.wsml.reasoner.normalization.AxiomatizationNormalizer;
-import org.omwg.logexpression.LogicalExpressionFactory;
+import org.deri.wsml.reasoner.normalization.OntologyNormalizer;
 import org.omwg.ontology.Attribute;
 import org.omwg.ontology.Concept;
 import org.omwg.ontology.Ontology;
 import org.wsmo.common.TopEntity;
 import org.wsmo.factory.Factory;
-import org.wsmo.factory.WsmoFactory;
-import org.wsmo.wsml.Parser;
 import org.wsmo.wsml.Serializer;
 
-public class AxiomatizationNormalizerTest extends TestCase
+public class AxiomatizationNormalizerTest extends WSMLNormalizationTest
 {
-    protected static int ontologyCount = 1;
-    protected AxiomatizationNormalizer transformer;
-    protected WsmoFactory wsmoFactory;
-    protected LogicalExpressionFactory leFactory;
+    protected OntologyNormalizer transformer;
 
     @Override
     protected void setUp() throws Exception
     {
         super.setUp();
-        wsmoFactory = Factory.createWsmoFactory(null);
-        HashMap createParams = new HashMap();
-        createParams.put(Factory.PROVIDER_CLASS, "org.deri.wsmo4j.logexpression.LogicalExpressionFactoryImpl");
-        leFactory = (LogicalExpressionFactory)Factory.createLogicalExpressionFactory(createParams);
         transformer = new AxiomatizationNormalizer();
     }
 
@@ -128,24 +112,5 @@ public class AxiomatizationNormalizerTest extends TestCase
                         Pattern.DOTALL);
         Matcher matcher = pattern.matcher(leString);
         assertTrue(matcher.find());
-    }
-
-    private Ontology parseOntology(String fileName) throws Exception
-    {
-        Map createParams = new HashMap();
-        createParams.put(Parser.PARSER_WSMO_FACTORY, wsmoFactory);
-        createParams.put(Parser.PARSER_LE_FACTORY, leFactory);
-        Parser parser = Factory.createParser(createParams);
-        InputStream stream = new FileInputStream(fileName);
-        assertNotNull("access test WSML file", stream);
-        return (Ontology)parser.parse(new InputStreamReader(stream))[0];
-    }
-
-    private Ontology createOntology()
-    {
-        int ontologyNumber = ontologyCount++;
-        Ontology ontology = wsmoFactory.createOntology(wsmoFactory.createIRI("http://mu.org#ont" + Integer.toString(ontologyNumber)));
-        ontology.setDefaultNamespace(wsmoFactory.createIRI("http://mu." + Integer.toString(ontologyNumber) + ".org#"));
-        return ontology;
     }
 }
--- wsml2reasoner/trunk/test/normalization/ConstructReductionNormalizerTest.java
+++ wsml2reasoner/trunk/test/normalization/ConstructReductionNormalizerTest.java
@@ -1,41 +1,23 @@
 package normalization;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import junit.framework.TestCase;
-
 import org.deri.wsml.reasoner.normalization.LEConstructReductionNormalizer;
-import org.deri.wsml.reasoner.normalization.LELLoydToporNormalizer;
 import org.deri.wsml.reasoner.normalization.OntologyNormalizer;
-import org.omwg.logexpression.LogicalExpressionFactory;
 import org.omwg.ontology.Ontology;
 import org.wsmo.common.TopEntity;
 import org.wsmo.factory.Factory;
-import org.wsmo.factory.WsmoFactory;
-import org.wsmo.wsml.Parser;
 import org.wsmo.wsml.Serializer;
 
-public class ConstructReductionNormalizerTest extends TestCase
+public class ConstructReductionNormalizerTest extends WSMLNormalizationTest
 {
-    protected static int ontologyCount = 1;
     protected OntologyNormalizer normalizer;
-    protected WsmoFactory wsmoFactory;
-    protected LogicalExpressionFactory leFactory;
 
     @Override
     protected void setUp() throws Exception
     {
         super.setUp();
-        wsmoFactory = Factory.createWsmoFactory(null);
-        HashMap createParams = new HashMap();
-        createParams.put(Factory.PROVIDER_CLASS, "org.deri.wsmo4j.logexpression.LogicalExpressionFactoryImpl");
-        leFactory = (LogicalExpressionFactory)Factory.createLogicalExpressionFactory(createParams);
         normalizer = new LEConstructReductionNormalizer();
     }
 
@@ -60,28 +42,8 @@ public class ConstructReductionNormalizerTest extends TestCase
         Serializer serializer = Factory.createSerializer(null);
         serializer.serialize(new TopEntity[] { normOnt }, buf);
         String normString = buf.toString();
-System.out.println(normString);
-        Pattern pattern = Pattern.compile("\\?x\\[.*topping.*hasValue.*\\?(\\w+)\\]\\s*and\\s*naf\\s*\\?\\1\\s*memberOf\\s*..Topping", Pattern.DOTALL);
+        Pattern pattern = Pattern.compile(".*E.*impliedBy.*C.*or.*D.*impliedBy.*A.*and.*B.*and.*A.*and.*B.*impliedBy.*E.*impliedBy.*C.*or.*D.*", Pattern.DOTALL);
         Matcher matcher = pattern.matcher(normString);
         assertTrue(matcher.find());
-    }
-
-    private Ontology createOntology()
-    {
-        int ontologyNumber = ontologyCount++;
-        Ontology ontology = wsmoFactory.createOntology(wsmoFactory.createIRI("http://mu.org#ont" + Integer.toString(ontologyNumber)));
-        ontology.setDefaultNamespace(wsmoFactory.createIRI("http://mu." + Integer.toString(ontologyNumber) + ".org#"));
-        return ontology;
-    }
-    
-    private Ontology parseOntology(String fileName) throws Exception
-    {
-        Map createParams = new HashMap();
-        createParams.put(Parser.PARSER_WSMO_FACTORY, wsmoFactory);
-        createParams.put(Parser.PARSER_LE_FACTORY, leFactory);
-        Parser parser = Factory.createParser(createParams);
-        InputStream stream = new FileInputStream(fileName);
-        assertNotNull("access test WSML file", stream);
-        return (Ontology)parser.parse(new InputStreamReader(stream))[0];
     }
 }
