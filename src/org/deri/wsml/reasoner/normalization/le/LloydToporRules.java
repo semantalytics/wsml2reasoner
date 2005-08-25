fc8e46c145fd3db4e649b9c3b0b7483da2672cc3
gabor <gabor@32303699-5c3f-0410-bc21-d627dcbb3c28>
2005-08-25 15:14:00 +0000

    *** empty log message ***

--- src/org/deri/wsml/reasoner/normalization/le/LloydToporRules.java
+++ src/org/deri/wsml/reasoner/normalization/le/LloydToporRules.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.omwg.logexpression.Binary;
 import org.omwg.logexpression.CompoundExpression;
 import org.omwg.logexpression.LogicalExpression;
+import org.omwg.logexpression.Unary;
 
 public class LloydToporRules extends FixedNormalizationRules
 {
@@ -15,9 +16,11 @@ public class LloydToporRules extends FixedNormalizationRules
     {
         super();
         rules.add(new SplitDisjunctiveBody());
+        rules.add(new SplitConstraint());
         rules.add(new SplitConjunctiveHead());
         rules.add(new TransformNestedImplication());
         rules.add(new SplitConjunction());
+        rules.add(new TransformImplication());
     }
 
     public static LloydToporRules instantiate()
@@ -36,7 +39,7 @@ public class LloydToporRules extends FixedNormalizationRules
             try
             {
                 CompoundExpression compound = (CompoundExpression)expression;
-                if(compound.getOperator() == CompoundExpression.IMPLIEDBY)
+                if(compound.getOperator() == CompoundExpression.LP_IMPL)
                 {
                     CompoundExpression conjunction = (CompoundExpression)compound.getArgument(0);
                     if(conjunction.getOperator() == CompoundExpression.AND)
@@ -55,8 +58,8 @@ public class LloydToporRules extends FixedNormalizationRules
             Set<LogicalExpression> resultingExpressions = new HashSet<LogicalExpression>();
             Binary rule = (Binary)expression;
             Binary conjunction = (Binary)rule.getArgument(0);
-            resultingExpressions.add(leFactory.createBinary(CompoundExpression.IMPLIEDBY, conjunction.getArgument(0), rule.getArgument(1)));
-            resultingExpressions.add(leFactory.createBinary(CompoundExpression.IMPLIEDBY, conjunction.getArgument(1), rule.getArgument(1)));
+            resultingExpressions.add(leFactory.createBinary(CompoundExpression.LP_IMPL, conjunction.getArgument(0), rule.getArgument(1)));
+            resultingExpressions.add(leFactory.createBinary(CompoundExpression.LP_IMPL, conjunction.getArgument(1), rule.getArgument(1)));
             return resultingExpressions;
         }
     }
@@ -68,7 +71,7 @@ public class LloydToporRules extends FixedNormalizationRules
             try
             {
                 CompoundExpression compound = (CompoundExpression)expression;
-                if(compound.getOperator() == CompoundExpression.IMPLIEDBY)
+                if(compound.getOperator() == CompoundExpression.LP_IMPL)
                 {
                     CompoundExpression disjunction = (CompoundExpression)compound.getArgument(1);
                     if(disjunction.getOperator() == CompoundExpression.OR)
@@ -87,8 +90,8 @@ public class LloydToporRules extends FixedNormalizationRules
             Set<LogicalExpression> resultingExpressions = new HashSet<LogicalExpression>();
             Binary rule = (Binary)expression;
             Binary disjunction = (Binary)rule.getArgument(1);
-            resultingExpressions.add(leFactory.createBinary(CompoundExpression.IMPLIEDBY, rule.getArgument(0), disjunction.getArgument(0)));
-            resultingExpressions.add(leFactory.createBinary(CompoundExpression.IMPLIEDBY, rule.getArgument(0), disjunction.getArgument(1)));
+            resultingExpressions.add(leFactory.createBinary(CompoundExpression.LP_IMPL, rule.getArgument(0), disjunction.getArgument(0)));
+            resultingExpressions.add(leFactory.createBinary(CompoundExpression.LP_IMPL, rule.getArgument(0), disjunction.getArgument(1)));
             return resultingExpressions;
         }
     }
@@ -100,7 +103,7 @@ public class LloydToporRules extends FixedNormalizationRules
             try
             {
                 CompoundExpression compound = (CompoundExpression)expression;
-                if(compound.getOperator() == CompoundExpression.IMPLIEDBY)
+                if(compound.getOperator() == CompoundExpression.LP_IMPL)
                 {
                     CompoundExpression rule = (CompoundExpression)compound.getArgument(0);
                     if(rule.getOperator() == CompoundExpression.IMPLIEDBY)
@@ -120,7 +123,7 @@ public class LloydToporRules extends FixedNormalizationRules
             Binary rule = (Binary)expression;
             Binary innerRule = (Binary)rule.getArgument(0);
             Binary conjunction = leFactory.createBinary(CompoundExpression.AND, innerRule.getArgument(1), rule.getArgument(1));
-            resultingExpressions.add(leFactory.createBinary(CompoundExpression.IMPLIEDBY, innerRule.getArgument(0), conjunction));
+            resultingExpressions.add(leFactory.createBinary(CompoundExpression.LP_IMPL, innerRule.getArgument(0), conjunction));
             return resultingExpressions;
         }
     }
@@ -150,6 +153,65 @@ public class LloydToporRules extends FixedNormalizationRules
             LogicalExpression rightArg = conjunction.getArgument(1);
             resultingExpressions.add(leftArg);
             resultingExpressions.add(rightArg);
+            return resultingExpressions;
+        }
+    }
+
+    public class TransformImplication implements TransformationRule
+    {
+        public boolean isApplicable(LogicalExpression expression)
+        {
+            try
+            {
+                CompoundExpression compound = (CompoundExpression)expression;
+                if(compound.getOperator() == CompoundExpression.IMPLIEDBY)
+                {
+                    return true;
+                }
+            } catch(ClassCastException e)
+            {
+            }
+            return false;
+        }
+
+        public Set<LogicalExpression> apply(LogicalExpression expression)
+        {
+            Set<LogicalExpression> resultingExpressions = new HashSet<LogicalExpression>();
+            Binary implication = (Binary)expression;
+            Binary rule = leFactory.createBinary(CompoundExpression.LP_IMPL, implication.getArgument(0), implication.getArgument(1));
+            resultingExpressions.add(rule);
+            return resultingExpressions;
+        }
+    }
+    
+    public class SplitConstraint implements TransformationRule
+    {
+        public boolean isApplicable(LogicalExpression expression)
+        {
+            try
+            {
+                CompoundExpression compound = (CompoundExpression)expression;
+                if(compound.getOperator() == CompoundExpression.CONSTRAINT)
+                {
+                    CompoundExpression disjunction = (CompoundExpression)compound.getArgument(0);
+                    if(disjunction.getOperator() == CompoundExpression.OR)
+                    {
+                        return true;
+                    }
+                }
+            } catch(ClassCastException e)
+            {
+            }
+            return false;
+        }
+
+        public Set<LogicalExpression> apply(LogicalExpression expression)
+        {
+            Set<LogicalExpression> resultingExpressions = new HashSet<LogicalExpression>();
+            Unary constraint = (Unary)expression;
+            Binary disjunction = (Binary)constraint.getArgument(0);
+            resultingExpressions.add(leFactory.createUnary(CompoundExpression.CONSTRAINT, disjunction.getArgument(0)));
+            resultingExpressions.add(leFactory.createUnary(CompoundExpression.CONSTRAINT, disjunction.getArgument(1)));
             return resultingExpressions;
         }
     }
