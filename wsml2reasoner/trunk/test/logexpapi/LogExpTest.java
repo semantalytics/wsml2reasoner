c2fa56fe643da6bd8f64556b180a509e34714ab9
gabor <gabor@32303699-5c3f-0410-bc21-d627dcbb3c28>
2005-10-11 14:18:43 +0000

    *** empty log message ***

--- wsml2reasoner/trunk/test/logexpapi/LogExpTest.java
+++ wsml2reasoner/trunk/test/logexpapi/LogExpTest.java
@@ -18,123 +18,141 @@
  */
 package logexpapi;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import junit.framework.TestCase;
 
-import org.deri.wsmo4j.io.parser.wsml.LogExprParserImpl;
 import org.deri.wsmo4j.io.serializer.wsml.LogExprSerializerWSML;
-import org.omwg.logicalexpression.AttrSpecification;
+import org.omwg.logicalexpression.CompoundMolecule;
 import org.omwg.logicalexpression.LogicalExpression;
-import org.wsmo.factory.LogicalExpressionFactory;
 import org.omwg.logicalexpression.Molecule;
+import org.omwg.logicalexpression.SubConceptMolecule;
 import org.omwg.logicalexpression.terms.Term;
 import org.omwg.ontology.Ontology;
 import org.wsmo.common.IRI;
 import org.wsmo.common.Namespace;
 import org.wsmo.factory.Factory;
+import org.wsmo.factory.LogicalExpressionFactory;
 import org.wsmo.factory.WsmoFactory;
 
 public class LogExpTest extends TestCase {
 
-    private org.omwg.logicalexpression.io.Parser leParser = null;
-
-    private org.omwg.logicalexpression.io.Serializer logExprSerializer = null;
+    private LogExprSerializerWSML logExprSerializer = null;
 
     private LogicalExpressionFactory leFactory = null;
+
+    private WsmoFactory wsmoFactory = null;
+
+    private Ontology o = null;
 
     public static void main(String[] args) {
         junit.textui.TestRunner.run(LogExpTest.class);
     }
 
     public void testFact() throws Exception {
-        LogicalExpression qExpression = leParser.parse("arc(a,b)");
+        LogicalExpression qExpression = leFactory.createLogicalExpression(
+                "arc(a,b)", o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testRule() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("path(?x,?y) :- arc(?x,?z) and path(?z,?y).");
+        LogicalExpression qExpression = leFactory.createLogicalExpression(
+                "path(?x,?y) :- arc(?x,?z) and path(?z,?y).", o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testImpliedBy() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("path(?x,?y) impliedBy arc(?x,?z) and path(?z,?y).");
+        LogicalExpression qExpression = leFactory.createLogicalExpression(
+                "path(?x,?y) impliedBy arc(?x,?z) and path(?z,?y).", o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testMoleculeRule() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("?x[arc hasValue ?y] :- ?x[arc hasValue ?z] and ?z[path hasValue ?y].");
+        LogicalExpression qExpression = leFactory
+                .createLogicalExpression(
+                        "?x[arc hasValue ?y] :- ?x[arc hasValue ?z] and ?z[path hasValue ?y].",
+                        o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testContraint() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("!- ?x[arc hasValue ?z] and ?z[path hasValue ?y].");
+        LogicalExpression qExpression = leFactory.createLogicalExpression(
+                "!- ?x[arc hasValue ?z] and ?z[path hasValue ?y].", o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testComplexRuleOne() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("?y memberOf ?z impliedBy ?z memberOf ?x :- naf ?y[a hasValue ?x, start hasValue _date(2005,6,6,0,0), nr hasValue 10, name hasValue \"myName\"] and p(?x,?y)");
+        LogicalExpression qExpression = leFactory
+                .createLogicalExpression(
+                        "?y memberOf ?z impliedBy ?z memberOf ?x :- naf ?y[a hasValue ?x, start hasValue _date(2005,6,6,0,0), nr hasValue 10, name hasValue \"myName\"] and p(?x,?y)",
+                        o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testComplexRuleTwo() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("?y memberOf ?x impliedBy ?y memberOf ?z impliedBy ?z memberOf ?x :- naf ?y[a hasValue ?x, start hasValue _date(2005,6,6,0,0), nr hasValue 10, name hasValue \"myName\"] and p(?x,?y)");
+        LogicalExpression qExpression = leFactory
+                .createLogicalExpression(
+                        "?y memberOf ?x impliedBy ?y memberOf ?z impliedBy ?z memberOf ?x :- naf ?y[a hasValue ?x, start hasValue _date(2005,6,6,0,0), nr hasValue 10, name hasValue \"myName\"] and p(?x,?y)",
+                        o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testComplexRuleThree() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("?y memberOf ?x impliedBy (?y memberOf ?z impliedBy ?z memberOf ?x) :- naf ?y[a hasValue ?x, start hasValue _date(2005,6,6,0,0), nr hasValue 10, name hasValue \"myName\"] and p(?x,?y)");
+        LogicalExpression qExpression = leFactory
+                .createLogicalExpression(
+                        "?y memberOf ?x impliedBy (?y memberOf ?z impliedBy ?z memberOf ?x) :- naf ?y[a hasValue ?x, start hasValue _date(2005,6,6,0,0), nr hasValue 10, name hasValue \"myName\"] and p(?x,?y)",
+                        o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testComplexRule4() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("?y memberOf ?x impliedBy (?y memberOf ?z impliedBy ?z memberOf ?x) :- naf ?y[a hasValue ?x, start hasValue _date(2005,6,6,0,0), nr hasValue 10, name hasValue \"myName\"] and (p(?x,?y) or q(?x,?y))");
+        LogicalExpression qExpression = leFactory
+                .createLogicalExpression(
+                        "?y memberOf ?x impliedBy (?y memberOf ?z impliedBy ?z memberOf ?x) :- naf ?y[a hasValue ?x, start hasValue _date(2005,6,6,0,0), nr hasValue 10, name hasValue \"myName\"] and (p(?x,?y) or q(?x,?y))",
+                        o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testMultipleImpliedBy() throws Exception {
-        LogicalExpression qExpression = leParser
-                .parse("p(?x,?y) impliedBy q(?x,?y) impliedBy r(?x,?y)");
+        LogicalExpression qExpression = leFactory.createLogicalExpression(
+                "p(?x,?y) impliedBy q(?x,?y) impliedBy r(?x,?y)", o);
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testSubconceptOfMolecule() throws Exception {
-        LogicalExpression qExpression = leParser.parse("?x memberOf ?y");
+        LogicalExpression qExpression = leFactory
+                .createLogicalExpression("?x memberOf ?y");
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testUnificationFact() throws Exception {
-        LogicalExpression qExpression = leParser.parse("x=y"); // Atom with
+        LogicalExpression qExpression = leFactory.createLogicalExpression(
+                "x=y", o); // Atom with
         // Constants.EQUAL
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
@@ -142,7 +160,8 @@ public class LogExpTest extends TestCase {
     }
 
     public void testInequalityFact() throws Exception {
-        LogicalExpression qExpression = leParser.parse("x!=y"); // Atom with
+        LogicalExpression qExpression = leFactory.createLogicalExpression(
+                "x!=y", o); // Atom with
         // Constants.INEQUAL
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
@@ -150,106 +169,86 @@ public class LogExpTest extends TestCase {
     }
 
     public void testManySubconceptOfMolecule() throws Exception {
-        Term sub = leFactory.createIRI("sub1");
-        Term super1 = leFactory.createIRI("super1");
-        Term super2 = leFactory.createIRI("super2");
-        Set<Term> superConcepts = new HashSet<Term>();
-        superConcepts.add(super1);
-        superConcepts.add(super2);
-        Molecule qExpression = leFactory.createMolecule(sub, superConcepts,
-                null, null);
+        Term sub = wsmoFactory.createIRI("sub1");
+        Term super1 = wsmoFactory.createIRI("super1");
+        Term super2 = wsmoFactory.createIRI("super2");
+        List<SubConceptMolecule> molecules = new ArrayList<SubConceptMolecule>();
+        molecules.add(leFactory.createSubConceptMolecule(sub, super1));
+        molecules.add(leFactory.createSubConceptMolecule(sub, super2));
+        CompoundMolecule qExpression = leFactory
+                .createCompoundMolecule(molecules);
         System.out.println("Created fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testMixedfMolecule1() throws Exception {
-        Term sub = leFactory.createIRI("sub1");
-        Term super1 = leFactory.createIRI("super1");
-        Term super2 = leFactory.createIRI("super2");
-        Term a1 = leFactory.createIRI("a1");
-        Term v1 = leFactory.createIRI("v1");
-        Term a2 = leFactory.createIRI("a2");
-        Term v2 = leFactory.createIRI("v2");
-        Set<Term> superConcepts = new HashSet<Term>();
-        superConcepts.add(super1);
-        superConcepts.add(super2);
-        AttrSpecification spec1 = leFactory.createAttrSpecification(
-                AttrSpecification.ATTR_VALUE, a1, Collections.singleton(v1));
-        AttrSpecification spec2 = leFactory.createAttrSpecification(
-                AttrSpecification.ATTR_VALUE, a2, Collections.singleton(v2));
-        Set<AttrSpecification> attrSpecs = new HashSet<AttrSpecification>();
-        attrSpecs.add(spec1);
-        attrSpecs.add(spec2);
-        Molecule qExpression = leFactory.createMolecule(sub, superConcepts,
-                null, attrSpecs);
+        Term sub = wsmoFactory.createIRI("sub1");
+        Term super1 = wsmoFactory.createIRI("super1");
+        Term super2 = wsmoFactory.createIRI("super2");
+        Term a1 = wsmoFactory.createIRI("a1");
+        Term v1 = wsmoFactory.createIRI("v1");
+        Term a2 = wsmoFactory.createIRI("a2");
+        Term v2 = wsmoFactory.createIRI("v2");
+        List<Molecule> molecules = new ArrayList<Molecule>();
+        molecules.add(leFactory.createSubConceptMolecule(sub, super1));
+        molecules.add(leFactory.createSubConceptMolecule(sub, super2));
+        molecules.add(leFactory.createAttributeValue(sub, a1, v1));
+        molecules.add(leFactory.createAttributeValue(sub, a2, v2));
+        CompoundMolecule qExpression = leFactory
+                .createCompoundMolecule(molecules);
         System.out.println("Created fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testMixedfMolecule2() throws Exception {
-        Term sub = leFactory.createIRI("sub1");
-        Term meta1 = leFactory.createIRI("meta1");
-        Term meta2 = leFactory.createIRI("meta2");
-        Term a1 = leFactory.createIRI("a1");
-        Term v1 = leFactory.createIRI("v1");
-        Term a2 = leFactory.createIRI("a2");
-        Term v2 = leFactory.createIRI("v2");
-        Set<Term> metaConcepts = new HashSet<Term>();
-        metaConcepts.add(meta1);
-        metaConcepts.add(meta2);
-        AttrSpecification spec1 = leFactory.createAttrSpecification(
-                AttrSpecification.ATTR_VALUE, a1, Collections.singleton(v1));
-        AttrSpecification spec2 = leFactory.createAttrSpecification(
-                AttrSpecification.ATTR_VALUE, a2, Collections.singleton(v2));
-        Set<AttrSpecification> attrSpecs = new HashSet<AttrSpecification>();
-        attrSpecs.add(spec1);
-        attrSpecs.add(spec2);
-        Molecule qExpression = leFactory.createMolecule(sub, null,
-                metaConcepts, attrSpecs);
+        Term sub = wsmoFactory.createIRI("sub1");
+        Term meta1 = wsmoFactory.createIRI("meta1");
+        Term meta2 = wsmoFactory.createIRI("meta2");
+        Term a1 = wsmoFactory.createIRI("a1");
+        Term v1 = wsmoFactory.createIRI("v1");
+        Term a2 = wsmoFactory.createIRI("a2");
+        Term v2 = wsmoFactory.createIRI("v2");
+        List<Molecule> molecules = new ArrayList<Molecule>();
+        molecules.add(leFactory.createMemberShipMolecule(sub, meta1));
+        molecules.add(leFactory.createMemberShipMolecule(sub, meta2));
+        molecules.add(leFactory.createAttributeValue(sub, a1, v1));
+        molecules.add(leFactory.createAttributeValue(sub, a2, v2));
+        CompoundMolecule qExpression = leFactory
+                .createCompoundMolecule(molecules);
         System.out.println("Created fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
     public void testMixedfMolecule3() throws Exception {
-        Term sub = leFactory.createIRI("sub1");
-        Term super1 = leFactory.createIRI("super1");
-        Term super2 = leFactory.createIRI("super2");
-        Term a1 = leFactory.createIRI("a1");
-        Term v1 = leFactory.createIRI("v1");
-        Term a2 = leFactory.createIRI("a2");
-        Term v2 = leFactory.createIRI("v2");
-        Term implType = leFactory.createIRI("implied");
-        Term typeConstr = leFactory.createIRI("constrained");
-        Set<Term> superConcepts = new HashSet<Term>();
-        superConcepts.add(super1);
-        superConcepts.add(super2);
-        AttrSpecification spec1 = leFactory.createAttrSpecification(
-                AttrSpecification.ATTR_VALUE, a1, Collections.singleton(v1));
-        AttrSpecification spec2 = leFactory.createAttrSpecification(
-                AttrSpecification.ATTR_VALUE, a2, Collections.singleton(v2));
-        AttrSpecification spec3 = leFactory.createAttrSpecification(
-                AttrSpecification.ATTR_INFERENCE, a2, Collections
-                        .singleton(implType));
-        AttrSpecification spec4 = leFactory.createAttrSpecification(
-                AttrSpecification.ATTR_CONSTRAINT, a2, Collections
-                        .singleton(typeConstr));
-        Set<AttrSpecification> attrSpecs = new HashSet<AttrSpecification>();
-        attrSpecs.add(spec1);
-        attrSpecs.add(spec2);
-        attrSpecs.add(spec3);
-        attrSpecs.add(spec4);
-        Molecule qExpression = leFactory.createMolecule(sub, superConcepts,
-                null, attrSpecs);
+        Term sub = wsmoFactory.createIRI("sub1");
+        Term super1 = wsmoFactory.createIRI("super1");
+        Term super2 = wsmoFactory.createIRI("super2");
+        Term a1 = wsmoFactory.createIRI("a1");
+        Term v1 = wsmoFactory.createIRI("v1");
+        Term a2 = wsmoFactory.createIRI("a2");
+        Term v2 = wsmoFactory.createIRI("v2");
+        Term implType = wsmoFactory.createIRI("implied");
+        Term typeConstr = wsmoFactory.createIRI("constrained");
+        List<Molecule> molecules = new ArrayList<Molecule>();
+        molecules.add(leFactory.createSubConceptMolecule(sub, super1));
+        molecules.add(leFactory.createSubConceptMolecule(sub, super2));
+        molecules.add(leFactory.createAttributeValue(sub, a1, v1));
+        molecules.add(leFactory.createAttributeValue(sub, a2, v2));
+        molecules.add(leFactory.createAttributeInference(sub, a2, implType));
+        molecules.add(leFactory.createAttributeConstraint(sub, a2, typeConstr));
+        CompoundMolecule qExpression = leFactory
+                .createCompoundMolecule(molecules);
         System.out.println("Created fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
     }
 
-    public void testAnonymousIds() throws Exception{
-        LogicalExpression qExpression = leParser.parse("_# memberOf _#1 and _#1 subConceptOf _#");
+    public void testAnonymousIds() throws Exception {
+        LogicalExpression qExpression = leFactory
+                .createLogicalExpression("_# memberOf _#1 and _#1 subConceptOf _#");
         System.out.println("Parsed fact:");
         System.out.println(logExprSerializer.serialize(qExpression));
         System.out.println("-------");
@@ -260,15 +259,15 @@ public class LogExpTest extends TestCase {
         WsmoFactory wf = Factory.createWsmoFactory(null);
         IRI ontoIri = wf.createIRI("urn:test");
         Namespace ns = wf.createNamespace("ns", ontoIri);
-        Ontology o = wf.createOntology(ontoIri);
+        o = wf.createOntology(ontoIri);
         o.setDefaultNamespace(ns);
-        leParser = new LogExprParserImpl();
         logExprSerializer = new LogExprSerializerWSML(o);
         Map createParams = new HashMap();
         createParams.put(Factory.PROVIDER_CLASS,
                 "org.deri.wsmo4j.logexpression.LogicalExpressionFactoryImpl");
         leFactory = (LogicalExpressionFactory) Factory
-                .createLogicalExpressionFactory(createParams);
+                .createLogicalExpressionFactory(null);
+        wsmoFactory = Factory.createWsmoFactory(null);
     }
 
 }
