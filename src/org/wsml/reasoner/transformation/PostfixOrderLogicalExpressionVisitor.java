129222c7b79d7f9f71d5443d9ff024dfd64da993
gabor <gabor@32303699-5c3f-0410-bc21-d627dcbb3c28>
2005-10-11 12:38:29 +0000

    *** empty log message ***

--- src/org/wsml/reasoner/transformation/PostfixOrderLogicalExpressionVisitor.java
+++ src/org/wsml/reasoner/transformation/PostfixOrderLogicalExpressionVisitor.java
@@ -19,28 +19,30 @@
 
 package org.wsml.reasoner.transformation;
 
+import java.util.Iterator;
+
 import org.omwg.logicalexpression.*;
 
 /**
- * Implements a left-first, depth-first traversal over logical expressions.
- * The point in time when a node has to be handled according to the traversal order
+ * Implements a left-first, depth-first traversal over logical expressions. The
+ * point in time when a node has to be handled according to the traversal order
  * is defined by the postfix order.
  * 
- * Subclasses should be visitors of logical expressions that 
- * don't need to care about how to traverse nodes but overwrite the new kinds of 
- * methods whose default implementation is empty:
+ * Subclasses should be visitors of logical expressions that don't need to care
+ * about how to traverse nodes but overwrite the new kinds of methods whose
+ * default implementation is empty:
  * 
- * (1) 'handleXXX' methods, that are called by the abstract traversal implementation
- *  when the respective node is actually to be handeled (according to the 
- *  traversal order)
- *  
- * (2) 'enterXXX' methods, that are called by the abstract traversal implementation
- *  when the respective subtree rooted in the given node is entered (according to the 
- *  traversal order)
+ * (1) 'handleXXX' methods, that are called by the abstract traversal
+ * implementation when the respective node is actually to be handeled (according
+ * to the traversal order)
  * 
- * (3) 'leaveXXX' methods, that are called by the abstract traversal implementation
- *  when the respective subtree rooted in the given node is left (according to the 
- *  traversal order)
+ * (2) 'enterXXX' methods, that are called by the abstract traversal
+ * implementation when the respective subtree rooted in the given node is
+ * entered (according to the traversal order)
+ * 
+ * (3) 'leaveXXX' methods, that are called by the abstract traversal
+ * implementation when the respective subtree rooted in the given node is left
+ * (according to the traversal order)
  * 
  * @author Uwe Keller, DERI Innsbruck
  */
@@ -52,221 +54,307 @@ public abstract class PostfixOrderLogicalExpressionVisitor implements Visitor {
         leaveAtom(arg0);
     }
 
-    public void visitMolecule(Molecule arg0) {
-        enterMolecule(arg0);
-        handleMolecule(arg0);
-        leaveMolecule(arg0);
-    }
-
-    public void visitNeg(Unary arg0) {
-        enterNeg(arg0);
-        arg0.getArgument(0).accept(this);
-        handleNeg(arg0);
-        leaveNeg(arg0);
-    }
-
-    public void visitNaf(Unary arg0) {
-        enterNaf(arg0);
-        arg0.getArgument(0).accept(this);
-        handleNaf(arg0);
-        leaveNaf(arg0);
-    }
-
-    public void visitConstraint(Unary arg0) {
-        enterConstraint(arg0);
-        arg0.getArgument(0).accept(this);
-        handleConstraint(arg0);
-        leaveConstraint(arg0);
-    }
-
-    public void visitAnd(Binary arg0) {
-        enterAnd(arg0);
-        arg0.getArgument(0).accept(this);
-        arg0.getArgument(1).accept(this);
-        handleAnd(arg0);
-        leaveAnd(arg0);
-    }
-
-    public void visitOr(Binary arg0) {
-        enterOr(arg0);
-        arg0.getArgument(0).accept(this);
-        arg0.getArgument(1).accept(this);
-        handleOr(arg0);
-        leaveOr(arg0);
-    }
-
-    public void visitImpliedBy(Binary arg0) {
-        enterImpliedBy(arg0);
-        arg0.getArgument(0).accept(this);
-        arg0.getArgument(1).accept(this);
-        handleImpliedBy(arg0);
-        leaveImpliedBy(arg0);
-    }
-
-    public void visitImplies(Binary arg0) {
-        enterImplies(arg0);
-        arg0.getArgument(0).accept(this);
-        arg0.getArgument(1).accept(this);
-        handleImplies(arg0);
-        leaveImplies(arg0);
-    }
-
-    public void visitEquivalent(Binary arg0) {
-        enterEquivalent(arg0);
-        arg0.getArgument(0).accept(this);
-        arg0.getArgument(1).accept(this);
-        handleEquivalent(arg0);
-        leaveEquivalent(arg0);
-    }
-
-    public void visitImpliesLP(Binary arg0) {
-        enterImpliesLP(arg0);
-        arg0.getArgument(0).accept(this);
-        arg0.getArgument(1).accept(this);
-        handleImpliesLP(arg0);
-        leaveImpliesLP(arg0);
-    }
-
-    public void visitForall(Quantified arg0) {
-        enterForall(arg0);
-        arg0.getArgument(0).accept(this);
-        handleForall(arg0);
-        leaveForall(arg0);
-    }
-
-    public void visitExists(Quantified arg0) {
-        enterExists(arg0);
-        arg0.getArgument(0).accept(this);
-        handleExists(arg0);
-        leaveExists(arg0);
-    }
-
-    abstract public Object getSerializedObject();
-    
-    public void handleAtom(Atom arg0){}
-    public void handleMolecule(Molecule arg0){}
-    public void handleNeg(Unary arg0) {}
-    public void handleNaf(Unary arg0) {}
-    public void handleConstraint(Unary arg0) {}
-    public void handleAnd(Binary arg0) {}
-    public void handleOr(Binary arg0) {}
-    public void handleImpliedBy(Binary arg0) {}
-    public void handleImplies(Binary arg0) {}
-    public void handleEquivalent(Binary arg0) {}
-    public void handleImpliesLP(Binary arg0) {}
-    public void handleForall(Quantified arg0) {}
-    public void handleExists(Quantified arg0) {}
-    
-    public void enterAtom(Atom arg0){}
-    public void enterMolecule(Molecule arg0){}
-    public void enterNeg(Unary arg0) {}
-    public void enterNaf(Unary arg0) {}
-    public void enterConstraint(Unary arg0) {}
-    public void enterAnd(Binary arg0) {}
-    public void enterOr(Binary arg0) {}
-    public void enterImpliedBy(Binary arg0) {}
-    public void enterImplies(Binary arg0) {}
-    public void enterEquivalent(Binary arg0) {}
-    public void enterImpliesLP(Binary arg0) {}
-    public void enterForall(Quantified arg0) {}
-    public void enterExists(Quantified arg0) {}
-
-    public void leaveAtom(Atom arg0){}
-    public void leaveMolecule(Molecule arg0){}
-    public void leaveNeg(Unary arg0) {}
-    public void leaveNaf(Unary arg0) {}
-    public void leaveConstraint(Unary arg0) {}
-    public void leaveAnd(Binary arg0) {}
-    public void leaveOr(Binary arg0) {}
-    public void leaveImpliedBy(Binary arg0) {}
-    public void leaveImplies(Binary arg0) {}
-    public void leaveEquivalent(Binary arg0) {}
-    public void leaveImpliesLP(Binary arg0) {}
-    public void leaveForall(Quantified arg0) {}
-    public void leaveExists(Quantified arg0) {}
-
     public void visitCompoundMolecule(CompoundMolecule arg0) {
-        // TODO Auto-generated method stub
-        
+        enterCompoundMolecule(arg0);
+        visitOperands(arg0);
+        handleCompoundMolecule(arg0);
+        leaveCompoundMolecule(arg0);
     }
 
     public void visitSubConceptMolecule(SubConceptMolecule arg0) {
-        // TODO Auto-generated method stub
-        
+        enterSubConceptMolecule(arg0);
+        handleSubConceptMolecule(arg0);
+        leaveSubConceptMolecule(arg0);
+
     }
 
     public void visitMemberShipMolecule(MembershipMolecule arg0) {
-        // TODO Auto-generated method stub
-        
+        enterMemberShipMolecule(arg0);
+        handleMemberShipMolecule(arg0);
+        leaveMemberShipMolecule(arg0);
+
     }
 
     public void visitAttributeValueMolecule(AttributeValueMolecule arg0) {
-        // TODO Auto-generated method stub
-        
+        enterAttributeValueMolecule(arg0);
+        handleAttributeValueMolecule(arg0);
+        leaveAttributeValueMolecule(arg0);
+
     }
 
     public void visitAttributeContraintMolecule(AttributeConstraintMolecule arg0) {
-        // TODO Auto-generated method stub
-        
+        enterAttributeConstraintMolecule(arg0);
+        handleAttributeConstraintMolecule(arg0);
+        leaveAttributeConstraintMolecule(arg0);
+
     }
 
     public void visitAttributeInferenceMolecule(AttributeInferenceMolecule arg0) {
-        // TODO Auto-generated method stub
-        
+        enterAttributeInferenceMolecule(arg0);
+        handleAttributeInferenceMolecule(arg0);
+        leaveAttributeInferenceMolecule(arg0);
+
     }
 
     public void visitNegation(Negation arg0) {
-        // TODO Auto-generated method stub
-        
+        enterNegation(arg0);
+        visitOperands(arg0);
+        handleNegation(arg0);
+        leaveNegation(arg0);
+
     }
 
     public void visitNegationAsFailure(NegationAsFailure arg0) {
-        // TODO Auto-generated method stub
-        
+        enterNegationAsFailure(arg0);
+        visitOperands(arg0);
+        handleNegationAsFailure(arg0);
+        leaveNegationAsFailure(arg0);
+
     }
 
     public void visitConstraint(Constraint arg0) {
-        // TODO Auto-generated method stub
-        
+        enterConstraint(arg0);
+        visitOperands(arg0);
+        handleConstraint(arg0);
+        leaveConstraint(arg0);
+
     }
 
     public void visitConjunction(Conjunction arg0) {
-        // TODO Auto-generated method stub
-        
+        enterConjunction(arg0);
+        visitOperands(arg0);
+        handleConjunction(arg0);
+        leaveConjunction(arg0);
     }
 
     public void visitDisjunction(Disjunction arg0) {
-        // TODO Auto-generated method stub
-        
+        enterDisjunction(arg0);
+        visitOperands(arg0);
+        handleDisjunction(arg0);
+        leaveDisjunction(arg0);
+
     }
 
     public void visitInverseImplication(InverseImplication arg0) {
-        // TODO Auto-generated method stub
-        
+        enterInverseImplication(arg0);
+        visitOperands(arg0);
+        handleInverseImplication(arg0);
+        leaveInverseImplication(arg0);
+
     }
 
     public void visitImplication(Implication arg0) {
-        // TODO Auto-generated method stub
-        
+        enterImplication(arg0);
+        visitOperands(arg0);
+        handleImplication(arg0);
+        leaveImplication(arg0);
+
     }
 
     public void visitEquivalence(Equivalence arg0) {
-        // TODO Auto-generated method stub
-        
+        enterEquivalence(arg0);
+        visitOperands(arg0);
+        handleEquivalence(arg0);
+        leaveEquivalence(arg0);
+
     }
 
     public void visitLogicProgrammingRule(LogicProgrammingRule arg0) {
-        // TODO Auto-generated method stub
-        
+        enterLogicProgrammingRule(arg0);
+        visitOperands(arg0);
+        handleLogicProgrammingRule(arg0);
+        leaveLogicProgrammingRule(arg0);
+
     }
 
     public void visitUniversalQuantification(UniversalQuantification arg0) {
-        // TODO Auto-generated method stub
-        
+        enterUniversalQuantification(arg0);
+        visitOperands(arg0);
+        handleUniversalQuantification(arg0);
+        leaveUniversalQuantification(arg0);
+
     }
 
     public void visitExistentialQuantification(ExistentialQuantification arg0) {
-        // TODO Auto-generated method stub
-        
+        enterExistentialQuantification(arg0);
+        visitOperands(arg0);
+        handleExistentialQuantification(arg0);
+        leaveExistentialQuantification(arg0);
+
     }
+
+    private void visitOperands(CompoundExpression arg0) {
+        for (Iterator i = arg0.listOperands().iterator(); i.hasNext();) {
+            LogicalExpression operand = (LogicalExpression) i.next();
+            operand.accept(this);
+        }
+    }
+
+    abstract public Object getSerializedObject();
+
+    public void enterAtom(Atom arg0) {
+    }
+
+    public void handleAtom(Atom arg0) {
+    }
+
+    public void leaveAtom(Atom arg0) {
+    }
+
+    public void enterSubConceptMolecule(SubConceptMolecule arg0) {
+    }
+
+    public void handleSubConceptMolecule(SubConceptMolecule arg0) {
+    }
+
+    public void leaveSubConceptMolecule(SubConceptMolecule arg0) {
+    }
+
+    public void enterMemberShipMolecule(MembershipMolecule arg0) {
+    }
+
+    public void handleMemberShipMolecule(MembershipMolecule arg0) {
+    }
+
+    public void leaveMemberShipMolecule(MembershipMolecule arg0) {
+    }
+
+    public void enterAttributeValueMolecule(AttributeValueMolecule arg0) {
+    }
+
+    public void handleAttributeValueMolecule(AttributeValueMolecule arg0) {
+    }
+
+    public void leaveAttributeValueMolecule(AttributeValueMolecule arg0) {
+    }
+
+    public void enterAttributeConstraintMolecule(
+            AttributeConstraintMolecule arg0) {
+    }
+
+    public void handleAttributeConstraintMolecule(
+            AttributeConstraintMolecule arg0) {
+    }
+
+    public void leaveAttributeConstraintMolecule(
+            AttributeConstraintMolecule arg0) {
+    }
+
+    public void enterAttributeInferenceMolecule(AttributeInferenceMolecule arg0) {
+    }
+
+    public void handleAttributeInferenceMolecule(AttributeInferenceMolecule arg0) {
+    }
+
+    public void leaveAttributeInferenceMolecule(AttributeInferenceMolecule arg0) {
+    }
+
+    public void enterConjunction(Conjunction arg0) {
+    }
+
+    public void handleConjunction(Conjunction arg0) {
+    }
+
+    public void leaveConjunction(Conjunction arg0) {
+    }
+
+    public void enterDisjunction(Disjunction arg0) {
+    }
+
+    public void handleDisjunction(Disjunction arg0) {
+    }
+
+    public void leaveDisjunction(Disjunction arg0) {
+    }
+
+    public void enterCompoundMolecule(CompoundMolecule arg0) {
+    }
+
+    public void handleCompoundMolecule(CompoundMolecule arg0) {
+    }
+
+    public void leaveCompoundMolecule(CompoundMolecule arg0) {
+    }
+    
+    public void enterNegation(Negation arg0) {
+    }
+
+    public void handleNegation(Negation arg0) {
+    }
+
+    public void leaveNegation(Negation arg0) {
+    }
+    
+    public void enterNegationAsFailure(NegationAsFailure arg0) {
+    }
+
+    public void handleNegationAsFailure(NegationAsFailure arg0) {
+    }
+
+    public void leaveNegationAsFailure(NegationAsFailure arg0) {
+    }
+    
+    public void enterConstraint(Constraint arg0) {
+    }
+
+    public void handleConstraint(Constraint arg0) {
+    }
+
+    public void leaveConstraint(Constraint arg0) {
+    }
+    
+    public void enterInverseImplication(InverseImplication arg0) {
+    }
+
+    public void handleInverseImplication(InverseImplication arg0) {
+    }
+
+    public void leaveInverseImplication(InverseImplication arg0) {
+    }
+    
+    public void enterImplication(Implication arg0) {
+    }
+
+    public void handleImplication(Implication arg0) {
+    }
+
+    public void leaveImplication(Implication arg0) {
+    }
+    
+    public void enterEquivalence(Equivalence arg0) {
+    }
+
+    public void handleEquivalence(Equivalence arg0) {
+    }
+
+    public void leaveEquivalence(Equivalence arg0) {
+    }
+    
+    public void enterLogicProgrammingRule(LogicProgrammingRule arg0) {
+    }
+
+    public void handleLogicProgrammingRule(LogicProgrammingRule arg0) {
+    }
+
+    public void leaveLogicProgrammingRule(LogicProgrammingRule arg0) {
+    }
+    
+    public void enterUniversalQuantification(UniversalQuantification arg0) {
+    }
+
+    public void handleUniversalQuantification(UniversalQuantification arg0) {
+    }
+
+    public void leaveUniversalQuantification(UniversalQuantification arg0) {
+    }
+    
+    public void enterExistentialQuantification(ExistentialQuantification arg0) {
+    }
+
+    public void handleExistentialQuantification(ExistentialQuantification arg0) {
+    }
+
+    public void leaveExistentialQuantification(ExistentialQuantification arg0) {
+    }
+
 }
