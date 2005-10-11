7215d25f9e2c59cb2c2b1d53e75bb62d06491981
gabor <gabor@32303699-5c3f-0410-bc21-d627dcbb3c28>
2005-10-11 13:14:28 +0000

    *** empty log message ***

--- wsml2reasoner/trunk/src/org/wsml/reasoner/impl/LogicalExpressionVariableVisitor.java
+++ wsml2reasoner/trunk/src/org/wsml/reasoner/impl/LogicalExpressionVariableVisitor.java
@@ -21,293 +21,237 @@ package org.wsml.reasoner.impl;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.omwg.logicalexpression.Atom;
-import org.omwg.logicalexpression.Binary;
-import org.omwg.logicalexpression.LogicalExpression;
-import org.omwg.logicalexpression.Molecule;
-import org.omwg.logicalexpression.Quantified;
-import org.omwg.logicalexpression.Unary;
+import org.omwg.logicalexpression.*;
 import org.omwg.logicalexpression.terms.Term;
 import org.omwg.ontology.Variable;
 import org.wsml.reasoner.transformation.PostfixOrderLogicalExpressionVisitor;
 
-
-
-/** 
- * A visitor that analyses variable occurrences in logical expressions.
- * After visiting a logical expression le, it can give the set 
- * FreeVars(le) of free variables in le and the set BoundVars(le) of
- * bound variables in le.
+/**
+ * A visitor that analyses variable occurrences in logical expressions. After
+ * visiting a logical expression le, it can give the set FreeVars(le) of free
+ * variables in le and the set BoundVars(le) of bound variables in le.
  * 
  * @author Uwe Keller, DERI Innsbruck
+ * @author Gabor Nagypal, FZI
  */
 public class LogicalExpressionVariableVisitor extends
         PostfixOrderLogicalExpressionVisitor {
 
-    private Map<LogicalExpression,Set<Variable>> freeVars;
-    private Map<LogicalExpression,Set<Variable>> boundVars;
-    
+    private Map<LogicalExpression, Set<Variable>> freeVars;
+
+    private Map<LogicalExpression, Set<Variable>> boundVars;
+
     private TermVariableVisitor tvv;
-    
-    
+
     public LogicalExpressionVariableVisitor() {
         super();
         reset();
     }
 
     /**
-     * Resets the internal state of the visitor such that the object
-     * can be reused across several calls in the same thread.
-     *
+     * Resets the internal state of the visitor such that the object can be
+     * reused across several calls in the same thread.
+     * 
      */
-    public void reset(){
+    public void reset() {
         freeVars = new HashMap<LogicalExpression, Set<Variable>>();
         boundVars = new HashMap<LogicalExpression, Set<Variable>>();
         tvv = new TermVariableVisitor();
     }
-    
+
     /**
      * Not used!
+     * 
      * @return null.
      */
     public Object getSerializedObject() {
         return null;
     }
-    
+
     /**
-     * Returns the set of all free variables in the given subexpression of the 
+     * Returns the set of all free variables in the given subexpression of the
      * visited logical expression.
-     * @param some subexpression of the visited expression
-     * @return FreeVar(le) or null if le is not a subexpression of the visited expression.
+     * 
+     * @param some
+     *            subexpression of the visited expression
+     * @return FreeVar(le) or null if le is not a subexpression of the visited
+     *         expression.
      */
-    public Set<Variable> getFreeVariables(LogicalExpression le){
+    public Set<Variable> getFreeVariables(LogicalExpression le) {
         Set<Variable> result = freeVars.get(le);
         return result;
     }
-    
+
     /**
-     * Returns the set of all bound variables in the given subexpression of the 
+     * Returns the set of all bound variables in the given subexpression of the
      * visited logical expression.
-     * @param some subexpression of the visited expression
-     * @return FreeVar(le) or null if le is not a subexpression of the visited expression.
+     * 
+     * @param some
+     *            subexpression of the visited expression
+     * @return FreeVar(le) or null if le is not a subexpression of the visited
+     *         expression.
      */
-    public Set<Variable> getBoundVariables(LogicalExpression le){
+    public Set<Variable> getBoundVariables(LogicalExpression le) {
         Set<Variable> result = boundVars.get(le);
         return result;
     }
 
-   
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleAtom(org.omwg.logicalexpression.Atom)
      */
     @Override
     public void handleAtom(Atom arg0) {
         Set<Variable> fvs = new HashSet<Variable>();
         Set<Variable> bvs = new HashSet<Variable>();
-        
-        for(int i = 0; i < arg0.getArity(); i++){
+
+        for (int i = 0; i < arg0.getArity(); i++) {
             Term nextArg = arg0.getParameter(i);
-            internalDoTerm(nextArg,fvs,bvs);
+            internalDoTerm(nextArg, fvs, bvs);
         }
-        
+
         freeVars.put(arg0, fvs);
         boundVars.put(arg0, bvs);
     }
 
-    /* (non-Javadoc)
-     * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleConstraint(org.omwg.logicalexpression.Unary)
-     */
     @Override
-    public void handleConstraint(Unary arg0) {
-        internalDoUnary(arg0);
+    public void handleConstraint(Constraint arg0) {
+        internalDoCompound(arg0);
     }
 
-    /* (non-Javadoc)
-     * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleEquivalent(org.omwg.logicalexpression.Binary)
-     */
     @Override
-    public void handleEquivalent(Binary arg0) {
-        internalDoBinary(arg0);
+    public void handleEquivalence(Equivalence arg0) {
+        internalDoCompound(arg0);
     }
 
-    /* (non-Javadoc)
-     * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleExists(org.omwg.logicalexpression.Quantified)
-     */
     @Override
-    public void handleExists(Quantified arg0) {
+    public void handleExistentialQuantification(ExistentialQuantification arg0) {
         internalDoQuantified(arg0);
     }
 
-    /* (non-Javadoc)
-     * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleForall(org.omwg.logicalexpression.Quantified)
-     */
     @Override
-    public void handleForall(Quantified arg0) {
+    public void handleUniversalQuantification(UniversalQuantification arg0) {
         internalDoQuantified(arg0);
     }
 
-    /* (non-Javadoc)
-     * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleImpliedBy(org.omwg.logicalexpression.Binary)
-     */
     @Override
-    public void handleImpliedBy(Binary arg0) {
-        internalDoBinary(arg0);
+    public void handleImplication(Implication arg0) {
+        internalDoCompound(arg0);
     }
 
-    /* (non-Javadoc)
-     * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleImplies(org.omwg.logicalexpression.Binary)
-     */
     @Override
-    public void handleImplies(Binary arg0) {
-        internalDoBinary(arg0);
+    public void handleInverseImplication(InverseImplication arg0) {
+        internalDoCompound(arg0);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleImpliesLP(org.omwg.logicalexpression.Binary)
      */
     @Override
-    public void handleImpliesLP(Binary arg0) {
-        internalDoBinary(arg0);
+    public void handleLogicProgrammingRule(LogicProgrammingRule arg0) {
+        internalDoCompound(arg0);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleMolecule(org.omwg.logicalexpression.Molecule)
      */
     @Override
-    public void handleMolecule(Molecule arg0) {
+    public void handleCompoundMolecule(CompoundMolecule arg0) {
         Set<Variable> fvs = new HashSet<Variable>();
         Set<Variable> bvs = new HashSet<Variable>();
 
-        Term t = arg0.getTerm();
-        
-        internalDoTerm(t, fvs, bvs);
-       
-        Set superConcepts =   arg0.listSubConceptOf();
-        Set memberOfClasses = arg0.listMemberOf();
-        Set attributeSpecs =  arg0.listAttributeSpecifications();
-        
-        // Superconcepts part ...
-        
-        if (superConcepts != null && superConcepts.size() > 0) {
-            for (Object sc : superConcepts){
-                Term scTerm = (Term) sc;
-                internalDoTerm(scTerm, fvs, bvs);
+        // extract left term
+        List operands = arg0.listOperands();
+        if (operands.size() > 0) {
+            Molecule m = (Molecule) operands.get(0);
+            Term t = m.getLeftParameter();
+            internalDoTerm(t, fvs, bvs);
+        }
+
+        for (Iterator i = operands.iterator(); i.hasNext();) {
+            Molecule m = (Molecule) i.next();
+            internalDoTerm(m.getRightParameter(), fvs, bvs);
+            if (m instanceof AttributeMolecule) {
+                AttributeMolecule am = (AttributeMolecule) m;
+                internalDoTerm(am.getAttribute(), fvs, bvs);
             }
         }
-        
-        // MemberOf part ...
-        
-        if (memberOfClasses != null && memberOfClasses.size() > 0) {
-            for (Object moc : memberOfClasses){
-                Term mocTerm = (Term) moc;
-                internalDoTerm(mocTerm, fvs, bvs);
-            }
-        }
-        
-        // AttributeSpecs part ...
-        
-        if (attributeSpecs != null && attributeSpecs.size() > 0) {
-            for (Object attSpec : attributeSpecs){
-                internalDoAttributeSpecification((AttrSpecification) attSpec, fvs, bvs);
-            }
-        }
-        
-        
         freeVars.put(arg0, fvs);
         boundVars.put(arg0, bvs);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleNaf(org.omwg.logicalexpression.Unary)
      */
     @Override
-    public void handleNaf(Unary arg0) {
-        internalDoUnary(arg0);
+    public void handleNegationAsFailure(NegationAsFailure arg0) {
+        internalDoCompound(arg0);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleNeg(org.omwg.logicalexpression.Unary)
      */
     @Override
-    public void handleNeg(Unary arg0) {
-        internalDoUnary(arg0);
+    public void handleNegation(Negation arg0) {
+        internalDoCompound(arg0);
     }
 
-    /* (non-Javadoc)
-     * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleOr(org.omwg.logicalexpression.Binary)
-     */
     @Override
-    public void handleOr(Binary arg0) {
-        internalDoBinary(arg0);
-    }
-    
-   // Some helper methods
-    
-    /* (non-Javadoc)
-     * @see org.wsml.reasoner.normalization.PostfixOrderLogicalExpressionVisitor#handleAnd(org.omwg.logicalexpression.Binary)
-     */
-    @Override
-    public void handleAnd(Binary arg0) {
-        internalDoBinary(arg0);
+    public void handleDisjunction(Disjunction arg0) {
+        internalDoCompound(arg0);
     }
 
-    private void internalDoBinary(Binary arg0){
+    @Override
+    public void handleConjunction(Conjunction arg0) {
+        internalDoCompound(arg0);
+    }
+
+    private void internalDoCompound(CompoundExpression arg0) {
         Set<Variable> fvs = new HashSet<Variable>();
         Set<Variable> bvs = new HashSet<Variable>();
-        
-        fvs.addAll(freeVars.get(arg0.getArgument(0)));
-        fvs.addAll(freeVars.get(arg0.getArgument(1)));
-        
-        bvs.addAll(boundVars.get(arg0.getArgument(0)));
-        bvs.addAll(boundVars.get(arg0.getArgument(1)));
-        
-        
+
+        for (Iterator i = arg0.listOperands().iterator(); i.hasNext();) {
+            LogicalExpression expr = (LogicalExpression) i.next();
+            fvs.addAll(freeVars.get(expr));
+            bvs.addAll(freeVars.get(expr));
+        }
         freeVars.put(arg0, fvs);
         boundVars.put(arg0, bvs);
     }
-    
-    private void internalDoUnary(Unary arg0){
-        freeVars.put(arg0, freeVars.get(arg0.getArgument(0)));
-        boundVars.put(arg0, boundVars.get(arg0.getArgument(0)));
-    }
-    
-    private void internalDoQuantified(Quantified arg0){
+
+    private void internalDoQuantified(Quantified arg0) {
         Set<Variable> fvs = new HashSet<Variable>();
         Set<Variable> bvs = new HashSet<Variable>();
-             
-        fvs.addAll(freeVars.get(arg0.getArgument(0)));
+
+        fvs.addAll(freeVars.get(arg0.getOperand()));
         fvs.removeAll(arg0.listVariables());
-        
-        bvs.addAll(boundVars.get(arg0.getArgument(0)));
+
+        bvs.addAll(boundVars.get(arg0.getOperand()));
         bvs.addAll(arg0.listVariables());
-        
+
         freeVars.put(arg0, fvs);
         boundVars.put(arg0, bvs);
-        
+
     }
-    
-    private void internalDoTerm(Term t, Set<Variable> fvs, Set<Variable> bvs){
+
+    private void internalDoTerm(Term t, Set<Variable> fvs, Set<Variable> bvs) {
         tvv.reset();
         t.accept(tvv);
         fvs.addAll(tvv.getVariables());
     }
-    
-    private void internalDoAttributeSpecification(AttrSpecification attSpec, Set<Variable> fvs, Set<Variable> bvs){
-        Term theAttribute = attSpec.getName();
-        internalDoTerm(theAttribute, fvs, bvs);
-        
-        Set theValues = attSpec.listArguments();
-                
-        for (Object attVal : theValues){
-            Term valTerm = (Term) attVal;
-            internalDoTerm(valTerm, fvs, bvs);
-        }
-    }
-    
-    
 
-    
 }
--- wsml2reasoner/trunk/src/org/wsml/reasoner/transformation/PostfixOrderTermVisitor.java
+++ wsml2reasoner/trunk/src/org/wsml/reasoner/transformation/PostfixOrderTermVisitor.java
@@ -20,46 +20,50 @@
 package org.wsml.reasoner.transformation;
 
 import org.omwg.logicalexpression.terms.*;
-
-
+import org.omwg.ontology.ComplexDataValue;
+import org.omwg.ontology.SimpleDataValue;
+import org.omwg.ontology.Variable;
+import org.wsmo.common.IRI;
+import org.wsmo.common.UnnumberedAnonymousID;
 
 /**
- * Implements a left-first, depth-first traversal over terms.
- * The point in time when a node has to be handled according to the traversal order
- * is defined by the postfix order.
+ * Implements a left-first, depth-first traversal over terms. The point in time
+ * when a node has to be handled according to the traversal order is defined by
+ * the postfix order.
  * 
- * Subclasses should be visitors of terms that 
- * don't need to care about how to traverse nodes but overwrite the new kinds of 
- * methods whose default implementation is empty:
+ * Subclasses should be visitors of terms that don't need to care about how to
+ * traverse nodes but overwrite the new kinds of methods whose default
+ * implementation is empty:
  * 
- * (1) 'handleXXX' methods, that are called by the abstract traversal implementation
- *  when the respective node is actually to be handeled (according to the 
- *  traversal order)
- *  
- * (2) 'enterXXX' methods, that are called by the abstract traversal implementation
- *  when the respective subtree rooted in the given node is entered (according to the 
- *  traversal order)
+ * (1) 'handleXXX' methods, that are called by the abstract traversal
+ * implementation when the respective node is actually to be handeled (according
+ * to the traversal order)
  * 
- * (3) 'leaveXXX' methods, that are called by the abstract traversal implementation
- *  when the respective subtree rooted in the given node is left (according to the 
- *  traversal order)
- *  
+ * (2) 'enterXXX' methods, that are called by the abstract traversal
+ * implementation when the respective subtree rooted in the given node is
+ * entered (according to the traversal order)
+ * 
+ * (3) 'leaveXXX' methods, that are called by the abstract traversal
+ * implementation when the respective subtree rooted in the given node is left
+ * (according to the traversal order)
+ * 
  * @author Uwe Keller, DERI Innsbruck
  */
-public class PostfixOrderTermVisitor implements VisitorTerms {
+public class PostfixOrderTermVisitor implements Visitor {
 
-    /* (non-Javadoc)
-	 * @see org.omwg.logicalexpression.terms.VisitorTerms#getSerializedObject()
-	 * should be obsolate (next snapshot)
-	 * TODO remove!
-	 */
-	public Object getSerializedObject() {
-		return null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.omwg.logicalexpression.terms.VisitorTerms#getSerializedObject()
+     *      should be obsolate (next snapshot) TODO remove!
+     */
+    public Object getSerializedObject() {
+        return null;
+    }
 
-	public void visitConstructedTerm(ConstructedTerm arg0) {
+    public void visitConstructedTerm(ConstructedTerm arg0) {
         enterConstructedTerm(arg0);
-        for(int i=0; i < arg0.getArity(); i++){
+        for (int i = 0; i < arg0.getArity(); i++) {
             Term nextArg = arg0.getParameter(i);
             nextArg.accept(this);
         }
@@ -74,97 +78,103 @@ public class PostfixOrderTermVisitor implements VisitorTerms {
         leaveVariable(arg0);
     }
 
-    public void visitString(WSMLString arg0) {
-        enterString(arg0);
-        handleString(arg0);
-        leaveString(arg0);
-
-    }
-
-    public void visitInteger(WSMLInteger arg0) {
-        enterInteger(arg0);
-        handleInteger(arg0);
-        leaveInteger(arg0);
-
-    }
-
-    public void visitDecimal(WSMLDecimal arg0) {
-        enterDecimal(arg0);
-        handleDecimal(arg0);
-        leaveDecimal(arg0);
-    }
- 
-   
-    public void visitAnonymousID(UnNbAnonymousID arg0) {
-        enterAnonymousID(arg0);
-        handleAnonymousID(arg0);
-        leaveAnonymousID(arg0);        
-    }
-    
-    public void visitNbAnonymousID(NbAnonymousID arg0) {
-        enterNbAnonymousID(arg0);
-        handleNbAnonymousID(arg0);
-        leaveNbAnonymousID(arg0);
-    }
-
     public void visitIRI(IRI arg0) {
         enterIRI(arg0);
         handleIRI(arg0);
         leaveIRI(arg0);
     }
-    
+
+    public void visitSimpleDataValue(SimpleDataValue arg0) {
+        enterSimpleDataValue(arg0);
+        handleSimpleDataValue(arg0);
+        leaveSimpleDataValue(arg0);
+
+    }
+
+    public void visitComplexDataValue(ComplexDataValue arg0) {
+        enterComplexDataValue(arg0);
+        handleComplexDataValue(arg0);
+        leaveComplexDataValue(arg0);
+
+    }
+
+    public void visitUnnumberedID(UnnumberedAnonymousID arg0) {
+        enterUnnumberedID(arg0);
+        handleUnnumberedID(arg0);
+        leaveUnnumberedID(arg0);
+
+    }
+
+    public void visitNumberedID(NumberedAnonymousID arg0) {
+        enterNumberedID(arg0);
+        handleNumberedID(arg0);
+        leaveNumberedID(arg0);
+
+    }
+
     // -- Additional methods ...
-    
-    public void enterConstructedTerm(ConstructedTerm arg0) {}
-    
-    public void enterVariable(Variable arg0) {}
-    
-    public void enterString(WSMLString arg0) {}
-    
-    public void enterInteger(WSMLInteger arg0) {}
-    
-    public void enterDecimal(WSMLDecimal arg0) {}
 
-    public void enterAnonymousID(UnNbAnonymousID arg0) {}
+    public void enterConstructedTerm(ConstructedTerm arg0) {
+    }
 
-    public void enterNbAnonymousID(NbAnonymousID arg0) {}
+    public void handleConstructedTerm(ConstructedTerm arg0) {
+    }
 
-    public void enterIRI(IRI arg0) {}
-    
-    
-    
-    public void handleConstructedTerm(ConstructedTerm arg0) {}
-    
-    public void handleVariable(Variable arg0) {}
-    
-    public void handleString(WSMLString arg0) {}
-    
-    public void handleInteger(WSMLInteger arg0) {}
-    
-    public void handleDecimal(WSMLDecimal arg0) {}
+    public void leaveConstructedTerm(ConstructedTerm arg0) {
+    }
 
-    public void handleAnonymousID(UnNbAnonymousID arg0) {}
+    public void enterVariable(Variable arg0) {
+    }
 
-    public void handleNbAnonymousID(NbAnonymousID arg0) {}
+    public void handleVariable(Variable arg0) {
+    }
 
-    public void handleIRI(IRI arg0) {}
-    
-    
-    public void leaveConstructedTerm(ConstructedTerm arg0) {}
-    
-    public void leaveVariable(Variable arg0) {}
-    
-    public void leaveString(WSMLString arg0) {}
-    
-    public void leaveInteger(WSMLInteger arg0) {}
-    
-    public void leaveDecimal(WSMLDecimal arg0) {}
+    public void leaveVariable(Variable arg0) {
+    }
 
-    public void leaveAnonymousID(UnNbAnonymousID arg0) {}
+    public void enterIRI(IRI arg0) {
+    }
 
-    public void leaveNbAnonymousID(NbAnonymousID arg0) {}
+    public void handleIRI(IRI arg0) {
+    }
 
-    public void leaveIRI(IRI arg0) {}
+    public void leaveIRI(IRI arg0) {
+    }
 
-   
+    public void enterSimpleDataValue(SimpleDataValue arg0) {
+    }
+
+    public void handleSimpleDataValue(SimpleDataValue arg0) {
+    }
+
+    public void leaveSimpleDataValue(SimpleDataValue arg0) {
+    }
+
+    public void enterComplexDataValue(ComplexDataValue arg0) {
+    }
+
+    public void handleComplexDataValue(ComplexDataValue arg0) {
+    }
+
+    public void leaveComplexDataValue(ComplexDataValue arg0) {
+    }
+
+    public void enterUnnumberedID(UnnumberedAnonymousID arg0) {
+    }
+
+    public void handleUnnumberedID(UnnumberedAnonymousID arg0) {
+    }
+
+    public void leaveUnnumberedID(UnnumberedAnonymousID arg0) {
+    }
+
+    public void enterNumberedID(NumberedAnonymousID arg0) {
+    }
+
+    public void handleNumberedID(NumberedAnonymousID arg0) {
+    }
+
+    public void leaveNumberedID(NumberedAnonymousID arg0) {
+    }
+
 }
