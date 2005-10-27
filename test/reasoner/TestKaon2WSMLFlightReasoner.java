caf2d0838903bc500dd162d092eae98a655b39e1
stephan <stephan@32303699-5c3f-0410-bc21-d627dcbb3c28>
2005-10-27 11:46:50 +0000

    *** empty log message ***

--- test/reasoner/TestKaon2WSMLFlightReasoner.java
+++ test/reasoner/TestKaon2WSMLFlightReasoner.java
@@ -23,13 +23,12 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.omwg.logicalexpression.terms.Term;
-import org.omwg.ontology.Variable;
-import org.wsml.reasoner.api.queryanswering.VariableBinding;
-import org.wsml.reasoner.impl.VariableBindingImpl;
-
 import junit.framework.Test;
 import junit.framework.TestSuite;
+
+import org.omwg.logicalexpression.terms.Term;
+import org.omwg.ontology.Variable;
+
 import test.BaseReasonerTest;
 
 public class TestKaon2WSMLFlightReasoner extends BaseReasonerTest {
@@ -90,7 +89,7 @@ public class TestKaon2WSMLFlightReasoner extends BaseReasonerTest {
         System.out.println("Finished query.");
         
     }
-    
+
     public void testAragornIsElendilsHeir() throws Exception {
         String query = "Aragorn[hasAncestor hasValue Elendil]";
         Set<Map<Variable, Term>> expected = new HashSet<Map<Variable, Term>>();
