27e25c63e9e1722a53ec587722c3ae847d9020bf
nobody <nobody@32303699-5c3f-0410-bc21-d627dcbb3c28>
2005-11-08 09:06:35 +0000

    This commit was manufactured by cvs2svn to create tag 'mins-without-flogic'.

--- src/org/wsml/reasoner/api/WSMLCoreReasoner.java
+++
@@ -1,7 +0,0 @@
-package org.wsml.reasoner.api;
-
-import org.wsmo.common.IRI;
-
-public interface WSMLCoreReasoner extends WSMLReasoner{
-    public boolean entails(IRI baseOntologyID, IRI consequenceOntologyID);
-}
--- src/org/wsml/reasoner/api/WSMLFlightReasoner.java
+++
@@ -1,7 +0,0 @@
-package org.wsml.reasoner.api;
-
-import org.wsmo.common.IRI;
-
-public interface WSMLFlightReasoner extends WSMLReasoner{
-    public boolean isSatisfiable(IRI ontologyID);
-}
