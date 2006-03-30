c30ffb41c5dc2d90cf0392763f17f031ce90ac34
gabor <gabor@32303699-5c3f-0410-bc21-d627dcbb3c28>
2006-03-30 09:10:34 +0000

    Made API changes

---
+++ api/src/org/wsml/reasoner/api/inconsistency/AttributeTypeViolation.java
@@ -0,0 +1,42 @@
+package org.wsml.reasoner.api.inconsistency;
+
+import org.omwg.ontology.Attribute;
+import org.omwg.ontology.Concept;
+import org.omwg.ontology.Instance;
+import org.omwg.ontology.Type;
+import org.omwg.ontology.Value;
+
+public class AttributeTypeViolation extends ConsistencyViolation {
+    
+    private Instance instance;
+
+    private Value violatingValue;
+
+    private Attribute attribute;
+
+    private Type expectedType;
+
+    public Attribute getAttribute() {
+        return attribute;
+    }
+
+    public Type getExpectedType() {
+        return expectedType;
+    }
+
+    public Instance getInstance() {
+        return instance;
+    }
+   
+    public Value getViolatingValue() {
+        return violatingValue;
+    }
+
+    public AttributeTypeViolation(Instance instance, Value violatingValue, Attribute attribute, Type expectedType) {
+        this.instance = instance;
+        this.violatingValue = violatingValue;
+        this.attribute = attribute;
+        this.expectedType = expectedType;
+    }
+
+}
