d0af5ff0ca0d1e3a1d0735287b8e66a1629eff4c
graham <graham@32303699-5c3f-0410-bc21-d627dcbb3c28>
2006-04-20 14:19:11 +0000

    Bundle of WSML-Core test suites

---
+++ wsml2reasoner/trunk/test/reasoner/core/ReasonerCoreTests.java
@@ -0,0 +1,26 @@
+package reasoner.core;
+
+import test.BaseReasonerTest;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class ReasonerCoreTests {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	        junit.textui.TestRunner.run(suite());
+	}
+
+ 	public static Test suite(){
+        TestSuite suite = new TestSuite("Test suite for the WSML-Core reasoner");
+        BaseReasonerTest.evalMethod = 1;
+        //$JUnit-BEGIN$
+        suite.addTest(SimpleGraph.suite());
+        suite.addTest(SimpleGraph2.suite());
+        //$JUnit-END$
+        return suite;
+    }
+ 	
+}
