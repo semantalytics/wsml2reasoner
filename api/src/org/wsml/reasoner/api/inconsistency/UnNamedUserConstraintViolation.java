61f0ff0abf74fcc09c3ea9a43ed4cead397f2c95
hlausen <hlausen@32303699-5c3f-0410-bc21-d627dcbb3c28>
2006-05-03 13:55:20 +0000

    added unnamed user

---
+++ api/src/org/wsml/reasoner/api/inconsistency/UnNamedUserConstraintViolation.java
@@ -0,0 +1,16 @@
+package org.wsml.reasoner.api.inconsistency;
+
+import org.wsmo.common.*;
+
+public class UnNamedUserConstraintViolation extends UserConstraintViolation {
+
+
+    public UnNamedUserConstraintViolation(IRI ontologyIri) {
+        super(ontologyIri);
+    }
+
+    public String toString(){
+        return "User constraint Violation due to anonymous axiom : ";  
+    }
+    
+}
