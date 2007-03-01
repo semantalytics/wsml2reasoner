0d247cc6b89fc459671b7d52cc324e9bf6ac983f
nathalie <nathalie@32303699-5c3f-0410-bc21-d627dcbb3c28>
2007-03-01 11:42:55 +0000

    changed test and extended it to use both Pellet and KAON2

--- wsml2reasoner/trunk/test/reasoner/dl/OntologyRegistrationTest.java
+++ wsml2reasoner/trunk/test/reasoner/dl/OntologyRegistrationTest.java
@@ -20,30 +20,69 @@ package reasoner.dl;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
+import org.omwg.ontology.Concept;
 import org.omwg.ontology.Ontology;
 import org.wsml.reasoner.api.WSMLReasoner;
+import org.wsml.reasoner.api.WSMLReasonerFactory.BuiltInReasoner;
 import org.wsml.reasoner.impl.DefaultWSMLReasonerFactory;
+import org.wsml.reasoner.impl.WSMO4JManager;
 import org.wsmo.common.IRI;
 import org.wsmo.factory.Factory;
+import org.wsmo.factory.WsmoFactory;
 import org.wsmo.wsml.Parser;
 
 
 public class OntologyRegistrationTest extends TestCase {
 
-    private WSMLReasoner wsmlReasoner;
+	private WsmoFactory wsmoFactory = null;
+	
+    private WSMLReasoner wsmlReasoner = null;
 
-    private Parser parser; 
+    private Parser parser = null; 
+    
+    private HashMap<String, Object> params = null;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
+		WSMO4JManager wsmoManager = new WSMO4JManager();
+        wsmoFactory = wsmoManager.getWSMOFactory();
         wsmlReasoner = DefaultWSMLReasonerFactory.getFactory().createWSMLDLReasoner();
 		parser = Factory.createParser(null);
+		params = new HashMap<String, Object>();
+	}
+	
+	/**
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown(){
+        wsmoFactory=null;
+        wsmlReasoner=null;
+        parser=null;
+        System.gc();
+    }
+	
+	public void testRun() throws Exception {
+		/*
+		 * Register multiple ontologies at Pellet and KAON2 and do simple 
+		 * inference tests
+    	 */
+        // Pellet
+        params.put(DefaultWSMLReasonerFactory.PARAM_BUILT_IN_REASONER, BuiltInReasoner.PELLET);
+        wsmlReasoner = DefaultWSMLReasonerFactory.getFactory().createWSMLDLReasoner(params);
+        doTestMultipleOntologies();
+        System.gc();
+        // KAON2
+        params.put(DefaultWSMLReasonerFactory.PARAM_BUILT_IN_REASONER, BuiltInReasoner.KAON2);
+        wsmlReasoner = DefaultWSMLReasonerFactory.getFactory().createWSMLDLReasoner(params);
+        doTestMultipleOntologies();
 	}
 
-	public void testMultipleOntologies() throws Exception {
+	public void doTestMultipleOntologies() throws Exception {
 		
 		/*----------------------- register different ontologies --------------------*/
 		
@@ -69,7 +108,7 @@ public class OntologyRegistrationTest extends TestCase {
         
         // read test file and parse it 
         is = this.getClass().getClassLoader().getResourceAsStream(
-                "reasoner/dl/wsml2owlShortExample.wsml");
+                "reasoner/dl/cycleTestFile.wsml");
         assertNotNull(is);
         // assuming first topentity in file is an ontology  
         Ontology ontology3 = (Ontology)parser.parse(new InputStreamReader(is))[0]; 
@@ -79,21 +118,44 @@ public class OntologyRegistrationTest extends TestCase {
         
         /*----------------------- reason over different ontologies --------------------*/
         
-        System.out.println(ontology.getIdentifier().toString());
-        System.out.println("Concepts: " + wsmlReasoner.getAllConcepts(
-        		(IRI) ontology.getIdentifier()).size());
-        System.out.println("----------------------------------------------------");
-        System.out.println(ontology2.getIdentifier().toString());
-        System.out.println("Concepts: " + wsmlReasoner.getAllConcepts(
-        		(IRI) ontology2.getIdentifier()).size());
-        System.out.println("----------------------------------------------------");
-        System.out.println(ontology3.getIdentifier().toString());
-        System.out.println("Concepts: " + wsmlReasoner.getAllConcepts(
-        		(IRI) ontology3.getIdentifier()).size());
+        // Ontology 1
+//        System.out.println(ontology.getIdentifier().toString());
+        Set<Concept> set = null; 
+        set = wsmlReasoner.getAllConcepts((IRI) ontology.getIdentifier());
+        set.remove(wsmoFactory.createConcept(wsmoFactory.createIRI(
+				"http://www.w3.org/2002/07/owl#Thing")));
+        set.remove(wsmoFactory.createConcept(wsmoFactory.createIRI(
+				"http://www.w3.org/2002/07/owl#Nothing")));
+        for (Concept c : set) 
+        	System.out.println(c.getIdentifier().toString());
+        assertTrue (set.size() == 18);
+                
+        // Ontology 2
+//        System.out.println(ontology2.getIdentifier().toString());
+        set.clear();
+        set = wsmlReasoner.getAllConcepts(
+        		(IRI) ontology2.getIdentifier());
+        set.remove(wsmoFactory.createConcept(wsmoFactory.createIRI(
+				"http://www.w3.org/2002/07/owl#Thing")));
+        set.remove(wsmoFactory.createConcept(wsmoFactory.createIRI(
+				"http://www.w3.org/2002/07/owl#Nothing")));
+        assertTrue (set.size() == 5);
+        
+        // Ontology 3
+//        System.out.println(ontology3.getIdentifier().toString());
+        set.clear();
+        set = wsmlReasoner.getAllConcepts(
+        		(IRI) ontology3.getIdentifier());
+        set.remove(wsmoFactory.createConcept(wsmoFactory.createIRI(
+				"http://www.w3.org/2002/07/owl#Thing")));
+        set.remove(wsmoFactory.createConcept(wsmoFactory.createIRI(
+				"http://www.w3.org/2002/07/owl#Nothing")));
+        assertTrue (set.size() == 9);
         
         wsmlReasoner.deRegisterOntology((IRI) ontology.getIdentifier());
         try {
-        	System.out.println(wsmlReasoner.getAllConcepts((IRI) ontology.getIdentifier()));
+        	assertTrue(wsmlReasoner.getAllConcepts(
+        			(IRI) ontology.getIdentifier()).size() == 18);
         	fail("Should fail because ontology is not registrated any more");
         } catch (RuntimeException e) {
         	System.out.println(e.getMessage());
