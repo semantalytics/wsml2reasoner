b0914c6662e47eb77a977b8fb7f33708eec6b6f3
hlausen <hlausen@32303699-5c3f-0410-bc21-d627dcbb3c28>
2007-03-20 20:30:54 +0000

    here we are the first working version of WSML FOL reasoner able to do entailment check works at present via remote webservice

--- src/org/wsml/reasoner/builtin/tptp/TPTPFacade.java
+++ src/org/wsml/reasoner/builtin/tptp/TPTPFacade.java
@@ -18,6 +18,8 @@
  */
 package org.wsml.reasoner.builtin.tptp;
 
+import java.io.*;
+import java.net.*;
 import java.util.*;
 
 import org.apache.log4j.Logger;
@@ -32,28 +34,87 @@ import org.wsml.reasoner.impl.WSMO4JManager;
  * The wsmo4j interface to and from TPTP
  * </p>
  * <p>
- * $Id: TPTPFacade.java,v 1.1 2007-02-08 15:01:32 hlausen Exp $
+ * $Id: TPTPFacade.java,v 1.2 2007-03-20 20:30:53 hlausen Exp $
  * </p>
  * 
  * @author Holger Lausen
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class TPTPFacade implements FOLReasonerFacade {
 
     WSMO4JManager wsmo4jmanager;
     Logger log = Logger.getLogger(TPTPFacade.class);
     
+    private String httpAddress="http://dev1.deri.at/dont-treat-this-service-to-hard";
+    
     public TPTPFacade(WSMO4JManager manager){
         this.wsmo4jmanager=manager;
     }
     
+    public List<EntailmentType> checkEntailment(String ontologyIRI,
+            List<LogicalExpression> conjecture) {
+        
+        String ontology = convertedOntologies.get(ontologyIRI);
+        if (ontology==null) throw new RuntimeException("ontology not registered");
+        
+        TPTPLESerializeVisitor les = new TPTPLESerializeVisitor();
+        List<EntailmentType> results = new ArrayList<EntailmentType>();
+        for (LogicalExpression le : conjecture) {
+            le.accept(les);
+            String conjectureString = les.getSerializedObject();
+            log.debug("checking conjecture"+conjectureString);
+            conjectureString = "fof(id" + id++ + ",conjecture, ("
+                    + conjectureString + ")). \n";
+            // log.debug(newExpression);
+            results.add(invokeHttp(ontology + "\n" + conjectureString));
+        }
+        return results;
+    }
     
-    /**
-     * here sits the MEET!!!!
-     */
-    public List<EntailmentType> checkEntailment(String ontologyIRI, List<LogicalExpression> conjecture) {
-        // TODO Auto-generated method stub
-        return null;
+    private String encode(String str){
+        try {
+            return URLEncoder.encode(str, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return str;
+        }
+    }
+    
+    private EntailmentType invokeHttp(String stuff){
+            String data = encode("theory") + "="+ encode(stuff);
+            URL url;
+            EntailmentType result =EntailmentType.unkown;
+            try {
+                url = new URL(httpAddress);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("FOL Reasoner not correclty configured: '"+httpAddress+"' is not an URL");
+            }
+            try{
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(data);
+                wr.flush();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    log.debug(line);
+                    if (line.contains("Proof found")){
+                        result=EntailmentType.entailed;
+                    }
+                    if (line.contains("Ran out of time")){
+                        result=EntailmentType.unkown;
+                    }
+                    if (line.contains("Completion found")){
+                        result=EntailmentType.notEntailed;
+                    }
+                }
+                wr.close();
+                rd.close();
+            }catch (IOException io){
+                throw new RuntimeException("the remote reasoner did not respond:"+io,io);
+            }
+            return result;
     }
 
     public void deregister(String ontologyURI) throws ExternalToolException {
@@ -63,6 +124,8 @@ public class TPTPFacade implements FOLReasonerFacade {
 
     Map<String,String> convertedOntologies = new HashMap<String, String>();
     
+    
+    public static int id=0;
     /**
      * I guess the cleverest would be to make a file based storage
      * for the converted Ontologies, however for now h HasMap should
@@ -74,7 +137,9 @@ public class TPTPFacade implements FOLReasonerFacade {
         for (LogicalExpression le:expressions ){
             le.accept(les);
             String newExpression = les.getSerializedObject();
-            log.debug("CONVERTED EXPRESSION: "+newExpression);
+            newExpression = "fof(id"+id++ +",axiom, ("+newExpression +")). \n";
+            //log.debug(newExpression);
+//            System.out.print(newExpression);
             kb+=newExpression;
         }
         convertedOntologies.put(ontologyURI, kb);
