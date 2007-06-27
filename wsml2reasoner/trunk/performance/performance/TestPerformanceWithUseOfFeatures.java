0e809943259be82fc9ce092b64ddb7cb046eff50
nathalie <nathalie@32303699-5c3f-0410-bc21-d627dcbb3c28>
2007-06-27 10:19:35 +0000

    *** empty log message ***

--- wsml2reasoner/trunk/performance/performance/TestPerformanceWithUseOfFeatures.java
+++ wsml2reasoner/trunk/performance/performance/TestPerformanceWithUseOfFeatures.java
@@ -49,23 +49,23 @@ public class TestPerformanceWithUseOfFeatures {
        
 //        ex.doTestRun();
         
-    	ex.testSubconceptOntologies();
-    	ex.testDeepSubconceptOntologies();
-    	ex.testInstanceOntologies();
-    	ex.testInstanceANDsubconceptOntologies();
-    	ex.testInstanceANDdeepSubconceptOntologies();
-    	ex.testOfTypeOntologies();
-    	ex.testOfTypeANDsubconceptOntologies();
-    	ex.testCardinality01Ontologies();
-    	ex.testCardinality010Ontologies();
+//    	ex.testSubconceptOntologies();
+//    	ex.testDeepSubconceptOntologies();
+//    	ex.testInstanceOntologies();
+//    	ex.testInstanceANDsubconceptOntologies();
+//    	ex.testInstanceANDdeepSubconceptOntologies();
+//    	ex.testOfTypeOntologies();
+//    	ex.testOfTypeANDsubconceptOntologies();
+//    	ex.testCardinality01Ontologies();
+//    	ex.testCardinality010Ontologies();
     	ex.testCardinality1maxOntologies();
-    	ex.testInverseAttributeOntologies();
-    	ex.testTransitiveAttributeOntologies();
-    	ex.testSymmetricAttributeOntologies();
-    	ex.testReflexiveAttributeOntologies();
-    	ex.testLocallyStratifiedNegation();
-    	ex.testGloballyStratifiedNegation();
-    	ex.testBuiltInAttributeOntologies();
+//    	ex.testInverseAttributeOntologies();
+//    	ex.testTransitiveAttributeOntologies();
+//    	ex.testSymmetricAttributeOntologies();
+//    	ex.testReflexiveAttributeOntologies();
+//    	ex.testLocallyStratifiedNegation();
+//    	ex.testGloballyStratifiedNegation();
+//    	ex.testBuiltInAttributeOntologies();
     }
 
     /**
