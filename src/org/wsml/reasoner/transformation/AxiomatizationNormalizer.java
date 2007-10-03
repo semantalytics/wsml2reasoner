702d5af35af4c480706e045ef0ae10a7afaebb3c
nathalie <nathalie@32303699-5c3f-0410-bc21-d627dcbb3c28>
2007-10-03 14:58:42 +0000

    fixed problem due to former implementation of attribute inheritance handling

--- src/org/wsml/reasoner/transformation/AxiomatizationNormalizer.java
+++ src/org/wsml/reasoner/transformation/AxiomatizationNormalizer.java
@@ -242,15 +242,6 @@ public class AxiomatizationNormalizer implements OntologyNormalizer {
                     superconceptID));
             
             mentionedIRIs.add(superconceptID);
-            
-            //TODO - handle inherited attributes
-            
-//            // inheritance of attributes
-//            Set<Attribute> superAttr = superconcept.listAttributes();
-//            for (Attribute a : superAttr) {
-//            	resultExpressions.addAll(normalizeConceptAttribute(conceptID,
-//                        a));
-//            }
         }
 
         // process attributes:
