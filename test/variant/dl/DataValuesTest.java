b5ce846dd8776a822b7dd0c39060411ad38637c9
graham <graham@32303699-5c3f-0410-bc21-d627dcbb3c28>
2007-11-15 13:06:59 +0000

    fixed testIRI

--- test/variant/dl/DataValuesTest.java
+++ test/variant/dl/DataValuesTest.java
@@ -188,7 +188,7 @@ public class DataValuesTest extends BaseNormalizationTest {
 		serializer.serialize(owlOntology, writer, prefs);
 //		System.out.println(writer.toString());
 		assertTrue(writer.toString().contains(
-				"value(b:hasLocation a:testOntology))"));
+				"value(a:hasLocation b:testOntology))"));
         axiom.removeDefinition(le);
     }
     
