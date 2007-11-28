2ad92343c89001d01f89a6b7c1eea1a68fee1469
poettler_ric <poettler_ric@32303699-5c3f-0410-bc21-d627dcbb3c28>
2007-11-28 13:26:27 +0000

    Added some documentation and a new constructor to the InternalReasonerException.

--- api/src/org/wsml/reasoner/api/InternalReasonerException.java
+++ api/src/org/wsml/reasoner/api/InternalReasonerException.java
@@ -19,22 +19,48 @@
 
 package org.wsml.reasoner.api;
 
+/**
+ * <p>
+ * Exception to indicate, that something went wrong in the reasoner while 
+ * evaluation.
+ * </p>
+ */
 public class InternalReasonerException extends RuntimeException {
 
     /**
-     * Needed for Java 5
-     */
-    private static final long serialVersionUID = -1814208889596998279L;
+     * Id used by the java serialization to identify the version of the class.
+	 */
+	private static final long serialVersionUID = -2424081972392190338L;
 
+	/**
+	 * Creates a new exception with a message.
+	 * @param msg the exception message
+	 */
     public InternalReasonerException(String msg) {
         super(msg);
     }
     
+    /**
+     * Creates a new exception empty exception.
+     */
     public InternalReasonerException() {
         super();
     }
     
+    /**
+     * Creates a new exception with a cause.
+     * @param e the cause for this exception
+     */
     public InternalReasonerException(Throwable e) {
         super(e);
+    }
+    
+    /**
+     * Creates a new exception with a message and a cause.
+     * @param msg the exception message
+     * @param cause the cause for this exception
+     */
+    public InternalReasonerException(final String msg, final Throwable cause) {
+    	super(msg, cause);
     }
 }
