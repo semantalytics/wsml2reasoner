ba3f24d26da603dd6a2194609d2b430fe803ed05
morcen <morcen@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-08-11 14:42:11 +0000

    cleaned up the le package, seperated each of the bundles of rules into individual classes (all inner classdes removed) and added a test suite for one of the rule sets - this needs to be extended

---
+++ wsml2reasoner/trunk/src/org/wsml/reasoner/transformation/le/lloydtopor/TransformNestedImplication.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2006 Digital Enterprise Research Insitute (DERI) Innsbruck
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.wsml.reasoner.transformation.le.lloydtopor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.omwg.logicalexpression.Conjunction;
+import org.omwg.logicalexpression.InverseImplication;
+import org.omwg.logicalexpression.LogicProgrammingRule;
+import org.omwg.logicalexpression.LogicalExpression;
+import org.wsml.reasoner.impl.WSMO4JManager;
+import org.wsml.reasoner.transformation.le.TransformationRule;
+import org.wsmo.factory.LogicalExpressionFactory;
+
+
+public class TransformNestedImplication implements TransformationRule {
+    
+    private LogicalExpressionFactory leFactory;
+    
+    public TransformNestedImplication(WSMO4JManager wsmoManager){
+        this.leFactory = wsmoManager.getLogicalExpressionFactory();
+    }
+    
+    public boolean isApplicable(LogicalExpression expression) {
+        if (expression instanceof LogicProgrammingRule) {
+            return ((LogicProgrammingRule) expression).getLeftOperand() instanceof InverseImplication;
+        }
+        return false;
+    }
+
+    public Set<LogicalExpression> apply(LogicalExpression expression) {
+        Set<LogicalExpression> resultingExpressions = new HashSet<LogicalExpression>();
+        InverseImplication innerRule = (InverseImplication) ((LogicProgrammingRule) expression).getLeftOperand();
+        Conjunction conjunction = leFactory.createConjunction(innerRule.getRightOperand(), ((LogicProgrammingRule) expression).getRightOperand());
+        resultingExpressions.add(leFactory.createLogicProgrammingRule(innerRule.getLeftOperand(), conjunction));
+        return resultingExpressions;
+    }
+
+    public String toString() {
+        return "A1 impliedBy A2 :- B\n\t=>\n A1 :- A2 and B\n";
+    }
+}
---
+++ wsml2reasoner/trunk/src/org/wsml/reasoner/transformation/le/moleculedecomposition/AtomAnonymousIDRule.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2006 Digital Enterprise Research Insitute (DERI) Innsbruck
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.wsml.reasoner.transformation.le.moleculedecomposition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.omwg.logicalexpression.Atom;
+import org.omwg.logicalexpression.LogicalExpression;
+import org.omwg.logicalexpression.terms.Term;
+import org.wsml.reasoner.impl.WSMO4JManager;
+import org.wsml.reasoner.transformation.AnonymousIdTranslator;
+import org.wsml.reasoner.transformation.le.NormalizationRule;
+import org.wsmo.common.Identifier;
+import org.wsmo.factory.LogicalExpressionFactory;
+
+
+public class AtomAnonymousIDRule implements NormalizationRule {
+
+    private LogicalExpressionFactory leFactory;
+    private AnonymousIdTranslator anonymousIDTranslator;
+    
+    public AtomAnonymousIDRule(WSMO4JManager wsmoManager, AnonymousIdTranslator anonymousIDTranslator){
+        this.leFactory = wsmoManager.getLogicalExpressionFactory();
+        this.anonymousIDTranslator = anonymousIDTranslator;
+    }
+    
+    public LogicalExpression apply(LogicalExpression expression) {
+        Atom atom = (Atom) expression;
+        Identifier id = (Identifier) anonymousIDTranslator.translate(atom.getIdentifier());
+        List<Term> args = new ArrayList<Term>();
+        for (int i = 0; i < atom.getArity(); i++) {
+            Term term = atom.getParameter(i);
+            if (term instanceof Identifier) {
+                term = anonymousIDTranslator.translate(term);
+            }
+            args.add(term);
+        }
+        return leFactory.createAtom(id, args);
+    }
+
+    public boolean isApplicable(LogicalExpression expression) {
+        return expression instanceof Atom;
+    }
+}
