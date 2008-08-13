52b215e82febecd7865c71a0263ca25ad0b5638e
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-08-13 21:29:24 +0000

    Added file headers.

--- wsml2reasoner/trunk/functional-test/helper/CoreHelper.java
+++ wsml2reasoner/trunk/functional-test/helper/CoreHelper.java
@@ -1,3 +1,25 @@
+/*
+ * WSML2Reasoner
+ * An extensible framework for reasoning with WSML ontologies.
+ * 
+ * Copyright (C) 2008 Semantic Technology Institute (STI) Innsbruck, 
+ * University of Innsbruck, Technikerstrasse 21a, 6020 Innsbruck, Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
 package helper;
 
 import java.util.HashMap;
@@ -111,36 +133,6 @@ public class CoreHelper
 		return lpResults;
 	}
 	
-//    public static void checkResults( Set<Instance> actualResults, Set<Map<Variable, Term>> expectedResults )
-//    {
-//		Assert.assertEquals( expectedResults.size(), actualResults.size() );
-//		for (Map<Variable, Term> binding : expectedResults) {
-//			Assert.assertTrue("Result does not contain instance: " +
-//		    		binding, instanceCheckerDL(actualResults, binding));
-//		}
-//    }
-//	
-//    private static boolean instanceCheckerDL(Set<Instance> result, Map<Variable, Term> binding) {
-//		 for (Instance instance : result) {
-//	         if (binding.values().contains(instance.getIdentifier())){
-//	        	 return true;
-//	         }
-//	     }
-//		return false;
-//	}
-//    
-//    private static Set<Identifier> convertQueryResults( Set<Map<Variable, Term>> actualQueryResults )
-//    {
-//    	Set<Identifier> identifiers = new HashSet<Identifier>();
-//    	
-//    	for( Map<Variable, Term> binding : actualQueryResults )
-//    	{
-//    		
-//    	}
-//    	
-//    	return identifiers;
-//    }
-
     private static final WsmoFactory wsmoFactory;
     private static final LogicalExpressionFactory leFactory;
     private static final DataFactory dataFactory;
--- wsml2reasoner/trunk/functional-test/helper/Results.java
+++ wsml2reasoner/trunk/functional-test/helper/Results.java
@@ -1,3 +1,25 @@
+/*
+ * WSML2Reasoner
+ * An extensible framework for reasoning with WSML ontologies.
+ * 
+ * Copyright (C) 2008 Semantic Technology Institute (STI) Innsbruck, 
+ * University of Innsbruck, Technikerstrasse 21a, 6020 Innsbruck, Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
 package helper;
 
 import java.util.Arrays;
@@ -15,8 +37,17 @@ import org.wsmo.factory.DataFactory;
 import org.wsmo.factory.LogicalExpressionFactory;
 import org.wsmo.factory.WsmoFactory;
 
+/**
+ * Helper to build a result set from a query.
+ * This class is used in tests that need to compare the expected query result with
+ * the actual result. 
+ */
 public class Results
 {
+	/**
+	 * Create the result set.
+	 * @param variableNames One instance for each variable in each 'row' of the result.
+	 */
 	public Results( String...variableNames )
 	{
 		int count = variableNames.length;
@@ -27,16 +58,32 @@ public class Results
 			mVariables[ v ] = leFactory.createVariable( variableNames[ v ] );
 	}
 	
+	/**
+	 * Create a wsml boolean term. 
+	 * @param b The value of the term.
+	 * @return The wsml boolean term object.
+	 */
 	public static Term bool( boolean b )
 	{
 		return dataFactory.createWsmlBoolean( b );
 	}
 	
+	/**
+	 * Create an IRI.
+	 * @param value The value of the IRI.
+	 * @return The IRI object.
+	 */
 	public static IRI iri( String value )
 	{
 		return wsmoFactory.createIRI( value );
 	}
 	
+	/**
+	 * Create a constructed term object.
+	 * @param functionSymbolIri The IRI identifier of the constructed term.
+	 * @param terms The list of term values.
+	 * @return The new term object.
+	 */
 	public static ConstructedTerm fn( String functionSymbolIri, Term...terms )
 	{
 		List<Term> termList = Arrays.asList( terms );
@@ -44,6 +91,12 @@ public class Results
 		return leFactory.createConstructedTerm( wsmoFactory.createIRI( functionSymbolIri ), termList );
 	}
 	
+	/**
+	 * Add a list of terms to a binding (row of result).
+	 * Each term is bound to the corresponding variable as provided to the constructor and
+	 * are bound in the same order. 
+	 * @param terms The terms to be bound to variables.
+	 */
 	public void addBinding( Term...terms )
 	{
 		if( mVariables.length != terms.length )
@@ -57,6 +110,10 @@ public class Results
 		mResults.add( binding );
 	}
 	
+	/**
+	 * Get the result set proper.
+	 * @return The set of maps of variable-term bindings.
+	 */
 	public Set<Map<Variable, Term>> get()
 	{
 		return mResults;
@@ -72,7 +129,7 @@ public class Results
     private static final WSMO4JManager wsmoManager;
     
     static{
-// 	 Set up factories for creating WSML elements
+    	// 	 Set up factories for creating WSML elements
 	   	wsmoManager = new WSMO4JManager();
 	
 	   	leFactory = wsmoManager.getLogicalExpressionFactory();
