1efd07b64f2efce83d3461e1aba7a26507e43165
matpre <matpre@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-08-29 12:56:37 +0000

    changed MoleculeDecompositionRulesTest

--- wsml2reasoner/trunk/unit-test/org/wsml/reasoner/transformation/le/moleculedecomposition/MoleculeDecompositionRulesTest.java
+++ wsml2reasoner/trunk/unit-test/org/wsml/reasoner/transformation/le/moleculedecomposition/MoleculeDecompositionRulesTest.java
@@ -22,83 +22,27 @@
  */
 package org.wsml.reasoner.transformation.le.moleculedecomposition;
 
-import org.omwg.logicalexpression.LogicalExpression;
-import org.wsml.reasoner.impl.WSMO4JManager;
-import org.wsml.reasoner.transformation.le.LETestHelper;
-import org.wsml.reasoner.transformation.le.moleculedecomposition.MoleculeDecompositionRule;
-import org.wsmo.wsml.ParserException;
-
 import junit.framework.TestCase;
 
-public class MoleculeDecompositionRulesTest extends TestCase{
-	
-	private MoleculeDecompositionRule rule;
-	 // "X[A1,...,An]\n\t=>\n X[A1] and ... and X[An]\n"
-	
-	  public MoleculeDecompositionRulesTest() {
-	        super();
-	    }
-	    
-	    protected void setUp() throws Exception {
-	        super.setUp();
-	        WSMO4JManager wsmoManager = new WSMO4JManager();
-	        this.rule = new MoleculeDecompositionRule(wsmoManager);
-	    }
-	    
-	    public void testIsApplicable() throws ParserException {
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"(_#)")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"(_#, _#)")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"(_#, _#, _#)")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("(_\"urn:a\" and _\"urn:b\") :- _\"urn:c\" ")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\" or _\"urn:b\"")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\" and _\"urn:b\" and _\"urn:c\" or _\"urn:d\"")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\" and _\"urn:b\" implies _\"urn:c\" ")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("(_\"urn:a\" or _\"urn:b\" ) and _\"urn:c\" or  _\"urn:d\"")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("(_\"urn:a\" or _\"urn:b\") :- _\"urn:e\" and _\"urn:c\" and  _\"urn:d\"")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE(" _\"urn:a\" and _\"urn:b\" and _\"urn:c\" or _\"urn:f\" :- _\"urn:d\"")));   
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_# memberOf _#")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_# memberOf _\"urn:a\"")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_\"urn:b\" ofType _#]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_\"urn:b\" impliesType _#]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_\"urn:b\" hasValue _#]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_# ofType _\"urn:b\"]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_# impliesType _\"urn:b\"]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_# hasValue _\"urn:b\"]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_# ofType _#]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_# impliesType _#]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_# hasValue _#]"))); 
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\" subConceptOf _\"urn:b\" and _\"urn:a\"[_\"urn:c\" ofType _#]"))); 
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\" subConceptOf _\"urn:b\" and _\"urn:a\"[_\"urn:c\" ofType _\"urn:d\"]")));
-	        assertFalse(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _\"urn:d\"] and _\"urn:a\" subConceptOf _\"urn:b\"")));
-	        
-	        assertTrue(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _\"urn:d\"] subConceptOf _\"urn:b\" ")));
-	        assertTrue(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _#] subConceptOf _# ")));
-	        assertTrue(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _# , _\"urn:d\" impliesType _#] memberOf _# ")));
-	        assertTrue(rule.isApplicable(LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _# , _\"urn:d\" impliesType _#, _# hasValue _#] subConceptOf _# ")));
-	       
-	        
-	    }
-	    
-	    public void testApply() throws ParserException {
-	    	LogicalExpression in = LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _\"urn:d\"] subConceptOf _\"urn:b\" ");
-	    	LogicalExpression out = LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _\"urn:d\"] and _\"urn:a\" subConceptOf _\"urn:b\".");
-	        LogicalExpression result = rule.apply(in);       
-	        assertTrue(result.equals(out));
-	        
-	        in = LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _\"urn:d\"] memberOf _\"urn:b\" ");
-	        out = LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _\"urn:d\"] and _\"urn:a\" memberOf _\"urn:b\".");
-	        result = rule.apply(in);          
-	        assertTrue(result.equals(out));
-	        
-	        in = LETestHelper.buildLE("_\"urn:a\"[_\"urn:c\" ofType _# , _\"urn:d\" impliesType _#] memberOf _#");  
-	        result = rule.apply(in);       
-	        assertTrue(result.toString().contains("_\"urn:a\"[_\"urn:d\" impliesType _#]"));
-	        assertTrue(result.toString().contains("_\"urn:a\" memberOf _#"));
-	        assertTrue(result.toString().contains("_\"urn:a\"[_\"urn:c\" ofType _#]"));
-	      
+import org.wsml.reasoner.impl.WSMO4JManager;
+import org.wsmo.wsml.ParserException;
 
-	    }
-	
-	
+public class MoleculeDecompositionRulesTest extends TestCase {
+
+	private MoleculeDecompositionRules rules;
+
+	public MoleculeDecompositionRulesTest() {
+		super();
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		WSMO4JManager wsmoManager = new WSMO4JManager();
+		this.rules = new MoleculeDecompositionRules(wsmoManager);
+	}
+
+	public void testGetRules() throws ParserException {
+		assertEquals(3, rules.getRules().size());
+	}
 
 }
