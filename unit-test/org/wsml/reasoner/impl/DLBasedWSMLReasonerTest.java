86b2a5d38a24040c0e4f7aff4d821809d93a98fb
matpre <matpre@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-08-29 15:10:10 +0000

    DatalogBasedWSMLReasonerTest

--- unit-test/org/wsml/reasoner/impl/DLBasedWSMLReasonerTest.java
+++ unit-test/org/wsml/reasoner/impl/DLBasedWSMLReasonerTest.java
@@ -23,23 +23,30 @@
 
 package org.wsml.reasoner.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.omwg.ontology.Attribute;
 import org.omwg.ontology.Axiom;
+import org.omwg.ontology.Concept;
+import org.omwg.ontology.Instance;
 import org.omwg.ontology.Ontology;
 import org.wsml.reasoner.api.WSMLReasonerFactory;
 import org.wsml.reasoner.api.inconsistency.InconsistencyException;
 import org.wsml.reasoner.impl.DLBasedWSMLReasoner;
 import org.wsml.reasoner.impl.DefaultWSMLReasonerFactory;
 import org.wsml.reasoner.impl.WSMO4JManager;
-import org.wsml.reasoner.transformation.le.LETestHelper;
 import org.wsmo.common.IRI;
 import org.wsmo.common.exception.InvalidModelException;
 import org.wsmo.common.exception.SynchronisationException;
+import org.wsmo.factory.Factory;
 import org.wsmo.factory.LogicalExpressionFactory;
 import org.wsmo.factory.WsmoFactory;
+import org.wsmo.validator.ValidationError;
+import org.wsmo.validator.ValidationWarning;
+import org.wsmo.validator.WsmlValidator;
 import org.wsmo.wsml.ParserException;
 
 import junit.framework.TestCase;
@@ -66,8 +73,45 @@ public class DLBasedWSMLReasonerTest extends TestCase {
 		wsmoFactory = wsmoManager.getWSMOFactory();
         leFactory = wsmoManager.getLogicalExpressionFactory();
         
-        ontology = wsmoFactory.createOntology(wsmoFactory.createIRI(ns + "ont"));
+        // build up standard ontology
+        ontology = wsmoFactory.createOntology(wsmoFactory.createIRI(ns + "AboutHumans"));
         ontology.setDefaultNamespace(wsmoFactory.createIRI(ns));
+        
+        Concept humanConcept = wsmoFactory.createConcept(wsmoFactory.createIRI(ns + "urn://Human" ));
+        Attribute hasRelativeAttr = humanConcept.createAttribute(wsmoFactory.createIRI(ns + "urn://hasRelative"));
+//        hasRelativeAttr.setSymmetric(true);
+        hasRelativeAttr.addType(humanConcept);
+        Concept locationConcept = wsmoFactory.createConcept(wsmoFactory.createIRI(ns + "urn://Location"));
+        
+        Attribute hasParentAttr = humanConcept.createAttribute(wsmoFactory.createIRI(ns + "urn://hasParent"));
+        hasParentAttr.addType(humanConcept);
+        
+        Attribute livesAtAttr = humanConcept.createAttribute(wsmoFactory.createIRI(ns + "urn://livesAt"));
+        livesAtAttr.addType(locationConcept);
+        
+        Instance person1 = wsmoFactory.createInstance(wsmoFactory.createIRI("urn://Person1"), humanConcept);
+        Instance person2 = wsmoFactory.createInstance(wsmoFactory.createIRI("urn://Person2"), humanConcept);
+        Instance person3 = wsmoFactory.createInstance(wsmoFactory.createIRI("urn://Person3"), humanConcept);
+        person1.addAttributeValue(hasParentAttr.getIdentifier(), person2);
+        person1.addAttributeValue(hasParentAttr.getIdentifier(), wsmoFactory.createInstance(wsmoFactory.createAnonymousID()));
+        
+        Axiom person1LivesAx = wsmoFactory.createAxiom(wsmoFactory.createIRI(ns + "urn://person1LivesSomewhere"));
+        person1LivesAx.addDefinition(leFactory.createConjunction(leFactory.createAttributeValue(person1.getIdentifier(), livesAtAttr.getIdentifier(), leFactory.createAnonymousID((byte)1)) , leFactory.createMemberShipMolecule(leFactory.createAnonymousID((byte)1),locationConcept.getIdentifier())));
+        Axiom person2LivesAx = wsmoFactory.createAxiom(wsmoFactory.createIRI(ns + "urn://person2LivesSomewhere"));
+        person2LivesAx.addDefinition(leFactory.createConjunction(leFactory.createConjunction(leFactory.createConjunction(leFactory.createConjunction(leFactory.createAttributeValue(person2.getIdentifier(), livesAtAttr.getIdentifier(), leFactory.createAnonymousID((byte)1)), leFactory.createMemberShipMolecule(leFactory.createAnonymousID((byte)1), locationConcept.getIdentifier())), leFactory.createAttributeValue(person3.getIdentifier(),livesAtAttr.getIdentifier(), leFactory.createAnonymousID((byte)2))), leFactory.createMemberShipMolecule(leFactory.createAnonymousID((byte)2), locationConcept.getIdentifier())), leFactory.createAttributeValue(person3.getIdentifier(), livesAtAttr.getIdentifier(), wsmoFactory.createAnonymousID())));
+        
+        ontology.addConcept(humanConcept);
+        ontology.addConcept(locationConcept);
+        ontology.addInstance(person1);
+        ontology.addInstance(person2);
+        ontology.addAxiom(person1LivesAx);
+        ontology.addAxiom(person2LivesAx);
+        
+
+        ontology.addConcept(humanConcept);
+        System.out.println(ontology.listConcepts());
+       
+        
         
         Map<String, Object> params = new HashMap<String, Object>();
         params.put(WSMLReasonerFactory.PARAM_BUILT_IN_REASONER, WSMLReasonerFactory.BuiltInReasoner.PELLET);
@@ -75,14 +119,22 @@ public class DLBasedWSMLReasonerTest extends TestCase {
         reasoner =  (DLBasedWSMLReasoner) DefaultWSMLReasonerFactory.getFactory().createDLReasoner(params);
 		
 	}
+	public void test01() {
+		assertTrue(true);
+	}
 	
 	public void test() throws ParserException, SynchronisationException, InvalidModelException, InconsistencyException {
 		
-	    Axiom axiom1 = wsmoFactory.createAxiom(wsmoFactory.createIRI(ns + "axiom01"));
-		axiom1.addDefinition(LETestHelper.buildLE("_\"urn:a\" and _\"urn:b\""));
-		axiom1.addDefinition(LETestHelper.buildLE("_\"urn:a\" subConceptOf _\"urn:c\""));
 		
-		ontology.addAxiom(axiom1);
+		WsmlValidator validator = Factory.createWsmlValidator(null);
+		ArrayList<ValidationError> arError = new ArrayList<ValidationError>();
+		ArrayList<ValidationWarning> arWarn = new ArrayList<ValidationWarning>();
+		validator.isValid(ontology, "http://www.wsmo.org/wsml/wsml-syntax/wsml-dl",arError ,arWarn );
+		if(arError.size() != 0) {
+			for(ValidationError er : arError) {
+				System.out.println(er.toString());
+			}
+		}
 		
 		reasoner.registerOntology(ontology);
 		
