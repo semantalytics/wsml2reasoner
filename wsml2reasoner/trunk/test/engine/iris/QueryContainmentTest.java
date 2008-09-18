bb886ce2faf4315fe0d2607e79794817bb5c8c83
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-18 07:03:16 +0000

    Tidy up of data type conversion and re-introduction of query containment.

--- wsml2reasoner/trunk/test/engine/iris/QueryContainmentTest.java
+++ wsml2reasoner/trunk/test/engine/iris/QueryContainmentTest.java
@@ -1,13 +1,16 @@
 package engine.iris;
 
+import java.util.Map;
+import java.util.Set;
 import org.omwg.logicalexpression.LogicalExpression;
+import org.omwg.logicalexpression.terms.Term;
 import org.omwg.ontology.Ontology;
+import org.omwg.ontology.Variable;
 import org.wsml.reasoner.api.LPReasoner;
 import org.wsml.reasoner.api.WSMLReasonerFactory;
 import org.wsmo.factory.Factory;
 import org.wsmo.factory.LogicalExpressionFactory;
 import org.wsmo.wsml.Parser;
-
 import base.BaseReasonerTest;
 
 /**
@@ -109,60 +112,60 @@ public class QueryContainmentTest extends BaseReasonerTest {
         wsmlReasoner.deRegister();
 	}
 	
-//	public void testExtendedPluginQueryContainment() throws Exception {
-//		String ns = "http://queryContainment#";
-//        String test = "namespace _\"" + ns + "\" \n" +
-//                "ontology o \n" +
-//                "concept GM_Object " +
-//        		"hasSRS impliesType SRS " +
-//        		"concept Polygon subConceptOf GM_Object " +
-//        		"concept SRS " +
-//        		"concept projSRS subConceptOf SRS " +
-//        		"instance gk memberOf projSRS " +
-//        		"axiom gm_objectDefinition definedBy " +
-//        		"?x[hasSRS hasValue ?srs] implies " +
-//        		"?x memberOf GM_Object. \n" +
-//                "relation overlay/3 " +
-//        		"relation intersection/3 " +
-//        		"relation union/3 " +
-//        		"relation dihfference/3 " +
-//        		"relation symmetricDifference/3 " +
-//        		"axiom overlayDefinition definedBy " +
-//        		"intersection(?x,?y,?z) implies overlay(?x,?y,?z). " +
-//        		"union(?x,?y,?z) implies overlay(?x,?y,?z). " +
-//        		"difference(?x,?y,?z) implies overlay(?x,?y,?z). " +
-//        		"symmetricDifference(?x,?y,?z) implies overlay(?x,?y,?z). " +
-//        		"axiom symmetricDifferenceDefinition definedBy " +
-//        		"symmetricDifference(?x,?y,?z) implies symmetricDifference(?y,?x,?z). " +
-//        		"symmetricDifference(?x,?y,?z) impliedBy difference(?x,?y, ?xydif) and " +
-//        		"difference(?y,?x,?yxdif) and union(?xydif, ?yxdif,?z). \n";
-//        ontology = (Ontology) parser.parse(new StringBuffer(test))[0];
-//        wsmlReasoner = BaseReasonerTest.getReasoner();
-//        wsmlReasoner.registerOntology(ontology);
-//        
-//        // build queries
-//		LogicalExpression query1 = leFactory.createLogicalExpression(
-//				"?x[hasSRS hasValue gk] memberOf Polygon and " +
-//				"?y[hasSRS hasValue gk] memberOf Polygon.\n", ontology);	
-//        LogicalExpression query2 = leFactory.createLogicalExpression(
-//        		"?a[hasSRS hasValue ?refsys] memberOf GM_Object and " +
-//        		"?b[hasSRS hasValue ?refsys] memberOf GM_Object and " +
-//        		"?refsys memberOf projSRS.", ontology);
-//        
-//        // perform query containment check
-//        boolean check = wsmlReasoner.checkQueryContainment(query1, query2);
-//        assertTrue(check);
-//        
-//        wsmlReasoner.deRegister();
-//        wsmlReasoner.registerOntology(ontology);
-//        
-//        // get containment mapping result set
-//        Set<Map<Variable, Term>> result = wsmlReasoner.getQueryContainment(
-//        		query1, query2);
-////        System.out.println(result);
-//        
-//        wsmlReasoner.deRegister();
-//	}
+	public void testExtendedPluginQueryContainment() throws Exception {
+		String ns = "http://queryContainment#";
+        String test = "namespace _\"" + ns + "\" \n" +
+                "ontology o \n" +
+                "concept GM_Object " +
+        		"hasSRS impliesType SRS " +
+        		"concept Polygon subConceptOf GM_Object " +
+        		"concept SRS " +
+        		"concept projSRS subConceptOf SRS " +
+        		"instance gk memberOf projSRS " +
+        		"axiom gm_objectDefinition definedBy " +
+        		"?x[hasSRS hasValue ?srs] implies " +
+        		"?x memberOf GM_Object. \n" +
+                "relation overlay/3 " +
+        		"relation intersection/3 " +
+        		"relation union/3 " +
+        		"relation dihfference/3 " +
+        		"relation symmetricDifference/3 " +
+        		"axiom overlayDefinition definedBy " +
+        		"intersection(?x,?y,?z) implies overlay(?x,?y,?z). " +
+        		"union(?x,?y,?z) implies overlay(?x,?y,?z). " +
+        		"difference(?x,?y,?z) implies overlay(?x,?y,?z). " +
+        		"symmetricDifference(?x,?y,?z) implies overlay(?x,?y,?z). " +
+        		"axiom symmetricDifferenceDefinition definedBy " +
+        		"symmetricDifference(?x,?y,?z) implies symmetricDifference(?y,?x,?z). " +
+        		"symmetricDifference(?x,?y,?z) impliedBy difference(?x,?y, ?xydif) and " +
+        		"difference(?y,?x,?yxdif) and union(?xydif, ?yxdif,?z). \n";
+        ontology = (Ontology) parser.parse(new StringBuffer(test))[0];
+        wsmlReasoner = (LPReasoner) BaseReasonerTest.getReasoner();
+        wsmlReasoner.registerOntology(ontology);
+        
+        // build queries
+		LogicalExpression query1 = leFactory.createLogicalExpression(
+				"?x[hasSRS hasValue gk] memberOf Polygon and " +
+				"?y[hasSRS hasValue gk] memberOf Polygon.\n", ontology);	
+        LogicalExpression query2 = leFactory.createLogicalExpression(
+        		"?a[hasSRS hasValue ?refsys] memberOf GM_Object and " +
+        		"?b[hasSRS hasValue ?refsys] memberOf GM_Object and " +
+        		"?refsys memberOf projSRS.", ontology);
+        
+        // perform query containment check
+        boolean check = wsmlReasoner.checkQueryContainment(query1, query2);
+        assertTrue(check);
+        
+        wsmlReasoner.deRegister();
+        wsmlReasoner.registerOntology(ontology);
+        
+        // get containment mapping result set
+        Set<Map<Variable, Term>> result = wsmlReasoner.getQueryContainment(
+        		query1, query2);
+//        System.out.println(result);
+        
+        wsmlReasoner.deRegister();
+	}
 	
 	public void testDisjunctiveQueryContainment() throws Exception {
 		String ns = "http://queryContainment#";
