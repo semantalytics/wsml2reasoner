de586d88840cb83839a0a00f06a5f7b917183211
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-19 20:06:46 +0000

    First version. Unit tests in advance of re-working relations in wsmo4j.

---
+++ wsml2reasoner/trunk/functional-test/concreteTests/irisStratified/Relations1SimpleNamedTest.java
@@ -0,0 +1,12 @@
+package concreteTests.irisStratified;
+
+import org.wsml.reasoner.api.LPReasoner;
+import abstractTests.lp.AbstractRelations1SimpleNamed;
+
+public class Relations1SimpleNamedTest extends AbstractRelations1SimpleNamed
+{
+	public LPReasoner getLPReasoner()
+	{
+		return IrisHelper.getReasoner();
+	}
+}
