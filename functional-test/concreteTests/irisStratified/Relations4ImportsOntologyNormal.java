13fb02cc26ad8bb2536451f3f19b1dab224d9cca
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-20 07:38:59 +0000

    Split this 'imports ontology with relations' test in to two tests.

--- functional-test/concreteTests/irisStratified/Relations4ImportsOntologyNormal.java
+++ functional-test/concreteTests/irisStratified/Relations4ImportsOntologyNormal.java
@@ -1,9 +1,9 @@
 package concreteTests.irisStratified;
 
 import org.wsml.reasoner.api.LPReasoner;
-import abstractTests.lp.AbstractRelations4ImportsOntology;
+import abstractTests.lp.AbstractRelations4ImportsOntologyNormal;
 
-public class Relations4ImportsOntologyTest extends AbstractRelations4ImportsOntology
+public class Relations4ImportsOntologyNormal extends AbstractRelations4ImportsOntologyNormal
 {
 	public LPReasoner getLPReasoner()
 	{
