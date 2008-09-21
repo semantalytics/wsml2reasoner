d19982e24a1e82e2080756aeca746cb899ba2067
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-21 23:13:06 +0000

    Relation test using out-of-order loading/parsing.

---
+++ wsml2reasoner/trunk/functional-test/abstractTests/lp/AbstractRelations5Example.java
@@ -0,0 +1,37 @@
+package abstractTests.lp;
+
+import helper.LPHelper;
+import helper.OntologyHelper;
+import helper.Results;
+import java.util.HashSet;
+import java.util.Set;
+import junit.framework.TestCase;
+import org.omwg.ontology.Ontology;
+import abstractTests.LP;
+
+public abstract class AbstractRelations5Example extends TestCase implements LP {
+
+	public void testQuery() throws Exception
+	{
+		Set<Ontology> ontologies = new HashSet<Ontology>();
+
+		// This works when loading o2 before o1, but not the other way round. It should not matter. 
+		Ontology o1 = OntologyHelper.loadOntology( "files/relations5_instance.wsml" );
+		Ontology o2 = OntologyHelper.loadOntology( "files/relations5_sub.wsml" );
+		Ontology o3 = OntologyHelper.loadOntology( "files/relations5_super.wsml" );
+		ontologies.add( o1 );
+		ontologies.add( o2 );
+		ontologies.add( o3 );
+
+		String queryString = "_\"http://www.sukesh.com/example/SubOntology#loves\"(?x,?y)";
+    	Results r = new Results( "x", "y" );
+    	
+    	String NS = "http://sukesh.com/example/InstanceOntology#";
+
+    	r.addBinding( r.iri( NS + "Peter" ), r.iri( NS + "Mary" ) );
+
+//    	System.out.println( OntologyHelper.toString( LPHelper.executeQuery( ontologies, queryString, getLPReasoner() ) ) );
+//    	if(true)return;
+		LPHelper.executeQueryAndCheckResults( ontologies, queryString, r.get(), getLPReasoner() );
+	}
+}
