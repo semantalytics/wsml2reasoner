0631184e6c78c7140bb30459e1e43a86639409c0
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-21 22:59:50 +0000

    Another test moved from 'test'.

---
+++ wsml2reasoner/trunk/functional-test/concreteTests/irisWellFounded/Stratification1NotStratifiedTest.java
@@ -0,0 +1,12 @@
+package concreteTests.irisWellFounded;
+
+import org.wsml.reasoner.api.LPReasoner;
+import abstractTests.lp.AbstractStratification1NotStratified;
+
+public class Stratification1NotStratifiedTest extends AbstractStratification1NotStratified
+{
+	public LPReasoner getLPReasoner()
+	{
+		return IrisHelper.getReasoner();
+	}
+}
