d0cdb3a37b68495c038928fd102cb339e60a0b34
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-21 22:59:50 +0000

    Another test moved from 'test'.

---
+++ functional-test/concreteTests/mins/Stratification1NotStratifiedTest.java
@@ -0,0 +1,12 @@
+package concreteTests.mins;
+
+import org.wsml.reasoner.api.LPReasoner;
+import abstractTests.lp.AbstractStratification1NotStratified;
+
+public class Stratification1NotStratifiedTest extends AbstractStratification1NotStratified
+{
+	public LPReasoner getLPReasoner()
+	{
+		return MinsHelper.getReasoner();
+	}
+}
