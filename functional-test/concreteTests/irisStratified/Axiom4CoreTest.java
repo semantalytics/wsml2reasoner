f2815b7875a3c454ef5b3da4bf4507743b5146b5
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-22 08:25:36 +0000

    Tests for wsml-core logical expressions.

---
+++ functional-test/concreteTests/irisStratified/Axiom4CoreTest.java
@@ -0,0 +1,12 @@
+package concreteTests.irisStratified;
+
+import org.wsml.reasoner.api.WSMLReasoner;
+import abstractTests.lp.AbstractAxiom4Core;
+
+public class Axiom4CoreTest extends AbstractAxiom4Core
+{
+	public WSMLReasoner getReasoner()
+	{
+		return IrisHelper.getReasoner();
+	}
+}
