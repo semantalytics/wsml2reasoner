f87ad30d62dbb5a4c8cdf01fa31c85bf6c79ad90
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-22 19:24:49 +0000

    Moved this test.

---
+++ wsml2reasoner/trunk/functional-test/concreteTests/irisWellFounded/QueryContainment1Test.java
@@ -0,0 +1,12 @@
+package concreteTests.irisWellFounded;
+
+import org.wsml.reasoner.api.LPReasoner;
+import abstractTests.lp.AbstractQueryContainment1;
+
+public class QueryContainment1Test extends AbstractQueryContainment1
+{
+	public LPReasoner getLPReasoner()
+	{
+		return IrisHelper.getReasoner();
+	}
+}
