723750c11a62d9274d9dcd9c234c920f2d1e15ba
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-22 10:43:36 +0000

    To identify problems with implication in rule bodies (wsml-rule).

---
+++ functional-test/concreteTests/mins/Axiom6RuleImplicationsInBodyTest.java
@@ -0,0 +1,12 @@
+package concreteTests.mins;
+
+import org.wsml.reasoner.api.LPReasoner;
+import abstractTests.lp.AbstractAxiom6RuleImplicationsInBody;
+
+public class Axiom6RuleImplicationsInBodyTest extends AbstractAxiom6RuleImplicationsInBody
+{
+	public LPReasoner getLPReasoner()
+	{
+		return MinsHelper.getReasoner();
+	}
+}
