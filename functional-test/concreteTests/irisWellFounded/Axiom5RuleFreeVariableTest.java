107c60eeb218bcc2fed812904d0c572e0869f621
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-22 09:43:47 +0000

    Tests for WSML-Rule logical expressions.

---
+++ functional-test/concreteTests/irisWellFounded/Axiom5RuleFreeVariableTest.java
@@ -0,0 +1,12 @@
+package concreteTests.irisWellFounded;
+
+import org.wsml.reasoner.api.LPReasoner;
+import abstractTests.lp.AbstractAxiom5RuleFreeVariable;
+
+public class Axiom5RuleFreeVariableTest extends AbstractAxiom5RuleFreeVariable
+{
+	public LPReasoner getLPReasoner()
+	{
+		return IrisHelper.getReasoner();
+	}
+}
