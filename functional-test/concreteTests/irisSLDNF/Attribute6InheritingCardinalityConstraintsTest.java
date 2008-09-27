398d1c63f877112b9de65140c96dc322928944a7
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-09-27 15:51:14 +0000

    Yet another attribute test.

---
+++ functional-test/concreteTests/irisSLDNF/Attribute6InheritingCardinalityConstraintsTest.java
@@ -0,0 +1,33 @@
+/**
+ * WSML2Reasoner
+ * An extensible framework for reasoning with WSML ontologies.
+ * 
+ * Copyright (C) 2008 Semantic Technology Institute (STI) Innsbruck, 
+ * University of Innsbruck, Technikerstrasse 21a, 6020 Innsbruck, Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package concreteTests.irisSLDNF;
+
+import org.wsml.reasoner.api.LPReasoner;
+import abstractTests.lp.AbstractAttribute6InheritingCardinalityConstraints;
+
+public class Attribute6InheritingCardinalityConstraintsTest extends AbstractAttribute6InheritingCardinalityConstraints {
+
+	public LPReasoner getLPReasoner() {
+		return Reasoner.get();
+	}
+}
