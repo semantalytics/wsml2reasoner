2d8e96e485fca70510a44d5bbde6c96162897bda
bazbishop237 <bazbishop237@32303699-5c3f-0410-bc21-d627dcbb3c28>
2008-11-10 11:27:16 +0000

    More tests for relations.

---
+++ wsml2reasoner/trunk/functional-test/concreteTests/irisStratified/Relations8ConstrainedToMultipleTypesTest.java
@@ -0,0 +1,12 @@
+package concreteTests.irisStratified;
+
+import org.wsml.reasoner.api.LPReasoner;
+import abstractTests.lp.AbstractRelations8ConstrainedToMultipleTypes;
+
+public class Relations8ConstrainedToMultipleTypesTest extends AbstractRelations8ConstrainedToMultipleTypes
+{
+	public LPReasoner getLPReasoner()
+	{
+		return Reasoner.get();
+	}
+}
