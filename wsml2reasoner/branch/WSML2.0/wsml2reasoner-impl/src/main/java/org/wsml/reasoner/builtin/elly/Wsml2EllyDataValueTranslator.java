7d828cabfce2f11f3f3a21fa8102ffc9de9a9677
daniel_winkler <daniel_winkler@32303699-5c3f-0410-bc21-d627dcbb3c28>
2010-07-13 13:45:12 +0000

    Changed translator to use termhelper.

--- wsml2reasoner/branch/WSML2.0/wsml2reasoner-impl/src/main/java/org/wsml/reasoner/builtin/elly/Wsml2EllyDataValueTranslator.java
+++ wsml2reasoner/branch/WSML2.0/wsml2reasoner-impl/src/main/java/org/wsml/reasoner/builtin/elly/Wsml2EllyDataValueTranslator.java
@@ -1,17 +1,8 @@
 package org.wsml.reasoner.builtin.elly;
 
-import static org.deri.iris.factory.Factory.CONCRETE;
-import static org.deri.iris.factory.Factory.TERM;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.deri.iris.api.terms.IConcreteTerm;
-import org.omwg.ontology.ComplexDataValue;
 import org.omwg.ontology.DataValue;
-import org.omwg.ontology.WsmlDataType;
-import org.omwg.ontology.XmlSchemaDataType;
 import org.sti2.elly.terms.ConcreteTerm;
+import org.wsml.reasoner.builtin.iris.TermHelper;
 
 /**
  * Helper Class to convert data values to terms, copied from Wsml2Datalog translation.
@@ -20,148 +11,7 @@ import org.sti2.elly.terms.ConcreteTerm;
 public class Wsml2EllyDataValueTranslator {
 
 	static org.sti2.elly.api.terms.IConcreteTerm convertWsmo4jDataValueToEllyTerm(final DataValue v) {
-		return ConcreteTerm.fromIRISConcreteTerm(convertWsmo4jDataValueToIrisTerm(v));
+		return ConcreteTerm.fromIRISConcreteTerm(TermHelper.convertWsmo4jDataValueToIrisTerm(v));
 	}
 	
-	/**
-	 * Converts a wsmo4j DataValue to an iris ITerm.
-	 * 
-	 * @param v
-	 *            the wsmo4j value to convert
-	 * @return the corresponding ITerm implementation
-	 */
-	static IConcreteTerm convertWsmo4jDataValueToIrisTerm(final DataValue v) {
-		if (v == null) {
-			throw new NullPointerException("The data value must not be null");
-		}
-		final String t = v.getType().getIdentifier().toString();
-		if (t.equals(WsmlDataType.WSML_BASE64BINARY) || t.equals(XmlSchemaDataType.XSD_BASE64BINARY)) {
-			return CONCRETE.createBase64Binary(v.getValue().toString());
-		} else if (t.equals(WsmlDataType.WSML_BOOLEAN) || t.equals(XmlSchemaDataType.XSD_BOOLEAN)) {
-			return CONCRETE.createBoolean(Boolean.valueOf(v.getValue().toString()));
-		} else if (t.equals(WsmlDataType.WSML_DATE) || t.equals(XmlSchemaDataType.XSD_DATE)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			int length = cv.getArity();
-			return CONCRETE.createDate(getIntFromValue(cv, 0), getIntFromValue(cv, 1), getIntFromValue(cv, 2),
-					length > 3 ? getIntFromValue(cv, 3) : 0, length > 4 ? getIntFromValue(cv, 4) : 0);
-		} else if (t.equals(WsmlDataType.WSML_DATETIME) || t.equals(XmlSchemaDataType.XSD_DATETIME)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			int length = cv.getArity();
-			return CONCRETE.createDateTime(getIntFromValue(cv, 0), getIntFromValue(cv, 1), getIntFromValue(cv, 2),
-					getIntFromValue(cv, 3), getIntFromValue(cv, 4), getDoubleFromValue(cv, 5),
-					length > 6 ? getIntFromValue(cv, 6) : 0, length > 7 ? getIntFromValue(cv, 7) : 0);
-		} else if (t.equals(WsmlDataType.WSML_TIME) || t.equals(XmlSchemaDataType.XSD_TIME)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			int length = cv.getArity();
-			return CONCRETE.createTime(getIntFromValue(cv, 0), getIntFromValue(cv, 1), getDoubleFromValue(cv, 2),
-					length > 3 ? getIntFromValue(cv, 3) : 0, length > 4 ? getIntFromValue(cv, 4) : 0);
-		} else if (t.equals(WsmlDataType.WSML_DECIMAL) || t.equals(XmlSchemaDataType.XSD_DECIMAL)) {
-			return CONCRETE.createDecimal(Double.parseDouble(v.getValue().toString()));
-		} else if (t.equals(WsmlDataType.WSML_DOUBLE) || t.equals(XmlSchemaDataType.XSD_DOUBLE)) {
-			return CONCRETE.createDouble(Double.parseDouble(v.getValue().toString()));
-		} else if (t.equals(WsmlDataType.WSML_DURATION) || t.equals(XmlSchemaDataType.XSD_DURATION)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			
-			int signum = 0;
-			List<Integer> values = new ArrayList<Integer>();
-			for (int i = 0; i <= 4; ++i) {
-				int value = getIntFromValue(cv, i);
-				values.add(Math.abs(value)); // Add absolute value
-				
-				if (signum == 0) { // signum not set yet
-					if (value > 0) {
-						signum = +1;
-					} else if (value < 0) {
-						signum = -1;
-					}
-				}
-			}
-			double seconds = getDoubleFromValue(cv, 5);
-			if (signum == 0) { // signum not set yet
-				if (seconds > 0.0) {
-					signum = +1;
-				} else if (seconds < 0.0) {
-					signum = -1;
-				}
-			}
-			seconds = Math.abs(seconds); // Add absolute value
-			
-			return CONCRETE.createDuration(
-					signum >= 0,
-					values.get(0), values.get(1), values.get(2),
-					values.get(3), values.get(4), seconds);
-		} else if (t.equals(WsmlDataType.WSML_FLOAT) || t.equals(XmlSchemaDataType.XSD_FLOAT)) {
-			return CONCRETE.createFloat(Float.parseFloat(v.getValue().toString()));
-		} else if (t.equals(WsmlDataType.WSML_GDAY) || t.equals(XmlSchemaDataType.XSD_GDAY)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			return CONCRETE.createGDay(getIntFromValue(cv, 0));
-		} else if (t.equals(WsmlDataType.WSML_GMONTH) || t.equals(XmlSchemaDataType.XSD_GMONTH)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			return CONCRETE.createGMonth(getIntFromValue(cv, 0));
-		} else if (t.equals(WsmlDataType.WSML_GMONTHDAY) || t.equals(XmlSchemaDataType.XSD_GMONTHDAY)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			return CONCRETE.createGMonthDay(getIntFromValue(cv, 0), getIntFromValue(cv, 1));
-		} else if (t.equals(WsmlDataType.WSML_GYEAR) || t.equals(XmlSchemaDataType.XSD_GYEAR)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			return CONCRETE.createGYear(getIntFromValue(cv, 0));
-		} else if (t.equals(WsmlDataType.WSML_GYEARMONTH) || t.equals(XmlSchemaDataType.XSD_GYEARMONTH)) {
-			final ComplexDataValue cv = (ComplexDataValue) v;
-			return CONCRETE.createGYearMonth(getIntFromValue(cv, 0), getIntFromValue(cv, 1));
-		} else if (t.equals(WsmlDataType.WSML_HEXBINARY) || t.equals(XmlSchemaDataType.XSD_HEXBINARY)) {
-			return CONCRETE.createHexBinary(v.getValue().toString());
-		} else if (t.equals(WsmlDataType.WSML_INTEGER) || t.equals(XmlSchemaDataType.XSD_INTEGER)) {
-			return CONCRETE.createInteger(Integer.parseInt(v.toString()));
-		} else if (t.equals(WsmlDataType.WSML_STRING) || t.equals(XmlSchemaDataType.XSD_STRING)) {
-			return TERM.createString(v.toString());
-		}
-		throw new IllegalArgumentException("Can't convert a value of type " + t);
-	}
-
-	/**
-	 * Returns the integer value of a ComplexDataValue at a given position.
-	 * 
-	 * @param value
-	 *            the complex data value from where to get the int
-	 * @param pos
-	 *            the index of the integer
-	 * @return the extracted and converted integer
-	 */
-	private static int getIntFromValue(final ComplexDataValue value, int pos) {
-		assert value != null;
-		assert pos >= 0;
-
-		return Integer.parseInt(getFieldValue(value, pos));
-	}
-
-	/**
-	 * Get a double value from the specified position.
-	 * 
-	 * @param value
-	 *            The complex data value from which the double is extracted.
-	 * @param pos
-	 *            The zero-basd index of the desired value.
-	 * @return
-	 */
-	private static double getDoubleFromValue(final ComplexDataValue value, int pos) {
-		assert value != null;
-		assert pos >= 0;
-
-		return Double.parseDouble(getFieldValue(value, pos));
-	}
-
-	/**
-	 * Get a field of a complex value.
-	 * 
-	 * @param value
-	 *            The complex value
-	 * @param pos
-	 *            The position of the file (zero-based index)
-	 * @return The string-ised field value.
-	 */
-	private static String getFieldValue(final ComplexDataValue value, int pos) {
-		assert value != null;
-		assert pos >= 0;
-
-		return value.getArgumentValue((byte) pos).getValue().toString();
-	}
 }
