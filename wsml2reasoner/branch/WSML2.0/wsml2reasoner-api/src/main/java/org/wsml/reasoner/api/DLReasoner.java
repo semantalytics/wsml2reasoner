28bbb3f90126f723ca29149d62f61bc48d4407e0
daniel_winkler <daniel_winkler@32303699-5c3f-0410-bc21-d627dcbb3c28>
2010-07-29 12:39:58 +0000

    removed author.

--- wsml2reasoner/branch/WSML2.0/wsml2reasoner-api/src/main/java/org/wsml/reasoner/api/DLReasoner.java
+++ wsml2reasoner/branch/WSML2.0/wsml2reasoner-api/src/main/java/org/wsml/reasoner/api/DLReasoner.java
@@ -36,7 +36,6 @@ import org.wsmo.common.Identifier;
  * An interface for invoking a WSML-DL reasoner with a particular reasoning
  * task.
  * 
- * @author Nathalie Steinmetz, DERI Innsbruck
  */
 public interface DLReasoner extends WSMLReasoner {
 
