9491f4d2c7919fe5ca105505292d10f757f92391
lanzanasto <lanzanasto@32303699-5c3f-0410-bc21-d627dcbb3c28>
2012-08-27 11:03:07 +0000

    using Streaming IRIS v0.1.0, check event format (N3, RDF/XML)

--- wsml2reasoner-impl/src/main/java/org/wsml/reasoner/builtin/streamingiris/StreamingIrisInputServerThread.java
+++ wsml2reasoner-impl/src/main/java/org/wsml/reasoner/builtin/streamingiris/StreamingIrisInputServerThread.java
@@ -10,6 +10,7 @@ import org.wsml.reasoner.StreamingDatalogReasonerFacade;
 import org.wsmo.common.TopEntity;
 import org.wsmo.common.exception.InvalidModelException;
 import org.wsmo.factory.FactoryContainer;
+import org.wsmo.wsml.ParserException;
 
 import de.ifgi.envision.io.parser.rdf.RDFParser;
 import de.ifgi.envision.io.parser.rdf.RDFParser.Syntax;
@@ -48,31 +49,58 @@ public class StreamingIrisInputServerThread extends Thread {
 	public void run() {
 
 		try {
-			InputStream inputStream = socket.getInputStream();
+			InputStream inputStream;
+			inputStream = socket.getInputStream();
 
 			// TODO Norbert: get syntax of incoming facts (RDF/XML, N3)
 			Syntax syntax = Syntax.N3;
 
 			// create corresponding parser
 			RDFParser rdfParser = new RDFParser(syntax, factory);
-			final TopEntity[] identifiable = rdfParser
-					.parse(new InputStreamReader(inputStream));
+			InputStreamReader inputStreamReader = new InputStreamReader(
+					inputStream);
 
-			Ontology ontology = null;
-			if (identifiable.length > 0 && identifiable[0] instanceof Ontology) {
-				ontology = (Ontology) identifiable[0];
-				// add facts to facade
-				facade.addFacts(ontology);
-			} else {
-				System.out.println("First Element of file no ontology ");
+			try {
+				final TopEntity[] identifiable = rdfParser
+						.parse(inputStreamReader);
+				Ontology ontology = null;
+				if (identifiable.length > 0
+						&& identifiable[0] instanceof Ontology) {
+					ontology = (Ontology) identifiable[0];
+					// add facts to facade
+					facade.addFacts(ontology);
+				} else {
+					System.out.println("First Element of file no ontology ");
+				}
+			} catch (org.wsmo.wsml.ParserException e) {
+				syntax = Syntax.RDF_XML;
+
+				// create corresponding parser
+				rdfParser = new RDFParser(syntax, factory);
+				TopEntity[] identifiable;
+				try {
+					identifiable = rdfParser.parse(inputStreamReader);
+
+					Ontology ontology = null;
+					if (identifiable.length > 0
+							&& identifiable[0] instanceof Ontology) {
+						ontology = (Ontology) identifiable[0];
+						// add facts to facade
+						facade.addFacts(ontology);
+					} else {
+						System.out
+								.println("First Element of file no ontology ");
+					}
+				} catch (ParserException e1) {
+					e1.printStackTrace();
+				} catch (InvalidModelException e1) {
+					e1.printStackTrace();
+				}
+			} catch (InvalidModelException e) {
+				e.printStackTrace();
 			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (org.wsmo.wsml.ParserException e) {
-			e.printStackTrace();
-		} catch (InvalidModelException e) {
-			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		} finally {
 			try {
 				socket.close();
